685804ef29a80e999ba4600810595678b67a1d58
vladislav <vladislav.karnygin@gmail.com>
2021-10-12 19:45:16 +0300

    выполнил четвертое задание, удалил из TODO (071021)

---
+++ Lessons/October/071021/ex4.py
@@ -0,0 +1,7 @@
+print("please, enter the width of the garden (in ft.) - ")
+width = int(input())
+print("and length... (in ft. too)")
+length = int(input())
+ft = 43560
+acre = length * width / ft
+print(acre)
