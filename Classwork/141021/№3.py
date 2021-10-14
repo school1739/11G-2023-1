9c3728375ada97295fa67ce2cf914413a2e1ab5e
kolodiy <agres321@gmail.com>
2021-10-14 12:46:22 +0300

    Сделал №3 и удалил его из TODO

---
+++ Classwork/141021/№3.py
@@ -0,0 +1,4 @@
+print("Введите скольо вы  сувениров и безделушек.")
+_suvenir= int(input())
+_bezdel= int(input())
+print("Вес вышей покупки составляет:" + " " +str(round(75*_suvenir + 112*_bezdel,0)) + " " + "грамм")
