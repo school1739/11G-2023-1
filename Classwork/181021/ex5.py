4d6ec8b29d9522a67ffb30e29eff2ac0b265c4c6
Savva Baryshev <savvabaryshev@gmail.com>
2021-10-18 12:46:40 +0300

    Задание 5

---
+++ Classwork/181021/ex5.py
@@ -0,0 +1,27 @@
+s = input()
+
+if s == 'январь':
+    print(31)
+elif s == 'февраль':
+    print('28 или 29')
+elif s == 'март':
+    print(31)
+elif s == 'апрель':
+    print(30)
+elif s == 'май':
+    print(31)
+elif s == 'июнь':
+    print(30)
+elif s == 'июль':
+    print(31)
+elif s == 'август':
+    print(31)
+elif s == 'сентябрь':
+    print(30)
+elif s == 'октябрь':
+    print(31)
+elif s == 'ноябрь':
+    print(30)
+elif s == 'декабрь':
+    print(31)
+
