f51c005e07ff2292c75a0636d904a28b53e0a1c6
LenovoUser <gavrikovvalentin9@yandex.ru>
2021-10-18 14:27:06 +0300

    сделал задание 5

---
+++ Classwork/181021/ex5.py
@@ -0,0 +1,27 @@
+a=input("Введите название месяца (англ., со строчной буквы): ")
+if a=="january":
+    print("31")
+elif a=="february":
+    print("28/29")
+elif a=="march":
+    print("31")
+elif a=="april":
+    print("30")
+elif a=="may":
+    print("31")
+elif a=="june":
+    print("30")
+elif a=="july":
+    print("31")
+elif a=="august":
+    print("31")
+elif a=="september":
+    print("30")
+elif a=="october":
+    print("31")
+elif a=="november":
+    print("30")
+elif a=="december":
+    print("31")
+else:
+    print("Некорректно введен месяц")
