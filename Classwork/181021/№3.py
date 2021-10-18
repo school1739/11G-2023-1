bcab202b716d60bab88f4721ed5159de89d9a941
LenovoUser <for.programming.77@gmail.com>
2021-10-18 12:50:10 +0300

    Сделал №3 и удалил его из TODO(181021)

---
+++ Classwork/181021/№3.py
@@ -0,0 +1,7 @@
+verb = input("Введите букву латинского:")
+if verb == 'a' or verb =='e' or verb =='i' or verb =='o' or verb =='u':
+    print("Это гласная")
+elif verb == 'y':
+    print("Эта буква может быть гласной и согласной")
+else:
+    print("Буква согласная")
