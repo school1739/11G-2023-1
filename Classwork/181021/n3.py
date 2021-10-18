ac608feeab9b76f5c243fe3639a692dad26171f7
Kayutkin <glebstar28@gmail.com>
2021-10-18 12:48:17 +0300

    Выполнил задание 3

---
+++ Classwork/181021/n3.py
@@ -0,0 +1,8 @@
+zxc = int(input("Введите букву")
+if zxc == ('a' or 'e' or 'i' or 'o' or 'u'):
+    print("Гласная")
+elif zxc == 'y':
+    print("Эта буква может быть и гласной и согласная")
+else:
+    print("Буква согласная")
+
