79f5e7d03d4e4c744438bbb3f13a2f987c9f7d7c
LenovoUser <gavrikovvalentin9@yandex.ru>
2021-10-18 14:37:03 +0300

    задание 5 сделанно

---
+++ classwork/181021/ex5.py
@@ -0,0 +1,26 @@
+a = input("Введи ваш месяц")
+if a=="Январь":
+    print("31")
+elif a=="Февраль":
+    print("28 или 29")
+elif a=="Март":
+    print("31")
+elif a=="Апрель":
+    print("30")
+elif a=="Май":
+    print("31")
+elif a=="Июнь":
+    print("30")
+elif a=="Июль":
+    print("31")
+elif a=="Август":
+    print("31")
+elif a=="Сентябрь":
+    print("30")
+elif a=="Октябрь":
+    print("31")
+elif a=="Ноябрь":
+    print("30")
+elif a=="Декабрь":
+    print("31")
+
