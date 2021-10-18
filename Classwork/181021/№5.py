b3e12e2a68cbe6cb632736677260911ec46fa16b
LenovoUser <for.programming.77@gmail.com>
2021-10-18 14:16:22 +0300

    Сделал №5 и удалил его из TODO(181021)

---
+++ Classwork/181021/№5.py
@@ -0,0 +1,25 @@
+month = input("Введите назвние месяца с маленькой буквы:")
+if month == "январь":
+    print("31")
+elif month == "февраль":
+    print("28/29")
+elif month == "март":
+    print("31")
+elif month == "апрель":
+    print("30")
+elif month == "май":
+    print("31")
+elif month == "июнь":
+    print("30")
+elif month == "июль":
+    print("31")
+elif month == "август":
+    print("31")
+elif month == "сентябрь":
+    print("30")
+elif month == "октябрь":
+    print("31")
+elif month == "ноябрь":
+    print("30")
+else:
+    print("31")
