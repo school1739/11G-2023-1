c3c3dcfadb8b8a1390c8d75df085222ddb92c9e3
LenovoUser <gavrikovvalentin9@yandex.ru>
2021-10-18 14:14:52 +0300

    задание 4 сделанно

---
+++ classwork/181021/ex4.py
@@ -0,0 +1,19 @@
+number_of_sides = int(input("Ведите количество сторон вашей фигуры"))
+if number_of_sides ==3:
+    print("Ваша фигура - треугольник")
+elif number_of_sides ==4:
+    print("Ваша фигура - четырехугольник")
+elif number_of_sides ==5:
+    print("Ваша фигура - пятиугольник")
+elif number_of_sides ==6:
+    print("Ваша фигура - шестиугольник")
+elif number_of_sides ==7:
+    print("Ваша фигура - семиугольник")
+elif number_of_sides ==8:
+    print("Ваша фигура - восьмиугольник")
+elif number_of_sides ==9:
+    print("Ваша фигура - девятиугольник")
+elif number_of_sides ==10:
+    print("Ваша фигура - десятиугольник")
+else:
+    print("Введите число от 3 до 10")
