88a3cbd9b1317447bf1c69bd9b1c88b90ae9d168
vladislav <vladislav.karnygin@gmail.com>
2021-10-18 12:48:45 +0300

    выполнил четвертое задание и удалил из TODO (181021)

---
+++ classwork/October/181021/ex4.py
@@ -0,0 +1,19 @@
+number_of_sides = int(input("введите количество сторон вашей фигуры"))
+if number_of_sides == 3:
+    print("ваша фигура: треугольник")
+elif number_of_sides == 4:
+    print("ваша фигура: четырехугольник")
+elif number_of_sides == 5:
+    print("ваша фигура: пятиугольник")
+elif number_of_sides == 6:
+    print("ваша фигура: шестиугольник")
+elif number_of_sides == 7:
+    print("ваша фигура: семиугольник")
+elif number_of_sides == 8:
+    print("ваша фигура: восьмиугольник")
+elif number_of_sides == 9:
+    print("ваша фигура: девятиугольник")
+elif number_of_sides == 10:
+    print("ваша фигура: десятиугольник")
+else:
+    print("ваше число должно быть от 3 до 10!")
