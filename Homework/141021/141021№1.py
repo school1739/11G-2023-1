a1274e409376d433ee6588e86e9d2e0a8f563520
Павел Бурыкин <pashabur18@gmail.com>
2021-10-20 21:24:40 +0300

    Задание 1 и 3

---
+++ Homework/141021/141021№1.py
@@ -0,0 +1,5 @@
+a = int(input())
+h = a / 100 * 18
+n = a / 100 * 20
+print(h,"Чаевые")
+print(a + n,"Итог" )
---
+++ Homework/141021/141021№3.py
@@ -0,0 +1,6 @@
+c = 75
+b = 112
+C = int(input("Сувениры: "))
+B = int(input("Безделушки: "))
+S = C * c + B * b
+print(S)
