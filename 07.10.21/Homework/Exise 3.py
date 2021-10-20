3e656751be36adf20f6841c2aa17654e70c98101
Егор <ryzhkove789@gmail.com>
2021-10-20 22:02:41 +0300

    Выполнил последнее задание (5) + push

--- 07.10.21/Homework/Exise 3.py
+++ 07.10.21/Homework/Exise 3.py
@@ -1,6 +1,10 @@
-a = float(input('Введите длину комнаты: '))
-b = float(input('Введите ширину комнаты: '))
-c = float(input('Введите высоту потолков: '))
-          S = a * b
-          print("Площадь комнаты")
-          print("Площадь стен")
+print("please, enter the length of the room:")
+length = int(input())
+print("please, enter the width of the room:")
+width = int(input())
+print("please, enter the height of the room")
+height = int(input())
+wall_area = 2 * length * height + 2 * width * height
+room_area = width * length
+print("wall area:" + "%f" % wall_area)
+print("room area:" + "%f" % room_area)
