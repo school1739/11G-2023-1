73944ba4d754765f8427fbbbe719b12f2e9a97b4
Павел Бурыкин <pashabur18@gmail.com>
2021-10-20 22:05:20 +0300

    Задание 5

---
+++ Homework/141021/141021№5.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+sum = a + b
+raz = a - b
+proiz = a * b
+dell = a / b
+log ="?"
+step = a**b
+print("Сумма:",sum)
+print("Разность", raz)
+print("частное от деления", dell)
+print("Произведение" , proiz)
+print(log)
+print("Степень", step)
