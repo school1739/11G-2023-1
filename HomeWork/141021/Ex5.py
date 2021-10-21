0eb42cad21fb71ac40369948dcdb025d5caddd92
Enigma <killanite@mail.ru>
2021-10-21 10:55:15 +0300

    Выполнил задание 5 за 14.10.21

---
+++ HomeWork/141021/Ex5.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+sum = a + b
+raz = a - b
+pro = a * b
+de = a / b
+step = a ** b
+print("Сумма", sum)
+print("Частное от деления", de)
+print("Разность", raz)
+print("Произведение", pro)
+print("Степень", step)
