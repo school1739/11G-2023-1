ce3f0ae25210cd45e8cf10369d2aa9d7a3a8d50c
Kayutkin <glebstar28@gmail.com>
2021-10-21 11:22:04 +0300

    Задание 1

---
+++ Classwork/211021/ex1.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+
+year = int(input('Год: '))
+month = int(input('Месяц: '))
+day = int(input('День: '))
+
+date = datetime(year, month, day)
+next_date = date + timedelta(days=1)
+
+print(f'Следующий день: {next_date.strftime("%d.%m.%Y")}')
