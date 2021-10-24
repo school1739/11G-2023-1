53db8917bdf5bf0207946db1e17acf6ca4bc54e5
Никита Неделько <ggarbe0@gmail.com>
2021-10-24 17:35:17 +0300

    задания за 141021 сделаны

---
+++ homework/141021/ex1.py
@@ -0,0 +1,5 @@
+print("Введите сумму заказа")
+a=int(input())
+n=0.18*a
+c=0.18*a
+print("Налог:+"+ str(round(n,2)) + "\nИтог:" +" "+str(round(c+n+a,2)))
---
+++ homework/141021/ex2.py
@@ -0,0 +1,4 @@
+print("Введите значение")
+a=100
+b=sum(range(1, a+1))
+print(b)
