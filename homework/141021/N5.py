051ef40b5479333c05f00ef02fb4162419b90d56
max <zhabka2019@mail.ru>
2021-10-25 13:47:23 +0300

    сделал N5 141021

---
+++ homework/141021/N5.py
@@ -0,0 +1,10 @@
+import math
+a = int(input("Введите первое число"))
+b = int(input("Введите второе число"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(math.log10(a))
+print(a**b)
