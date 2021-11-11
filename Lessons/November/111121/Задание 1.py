c14a9f9c6cdbe86c9935b443ef5a16857b70c2b6
федор аниканов <anikanov.fedor.01@gmail.com>
2021-11-11 10:28:23 +0300

    Сделал задание 1

--- Lessons/November/111121/Задание 1.py
+++ Lessons/November/111121/Задание 1.py
@@ -1,1 +1,11 @@
+sum = 0
+i = 0
 n = int(input())
+if sum == 0 and i == 0 and n == 0:
+    print("Ошибка! 0 не может быть первым числом!")
+else:
+    while n != 0:
+        sum = sum + n
+        i = i + 1
+        n = int(input())
+print("%.2f" % (sum / i))
