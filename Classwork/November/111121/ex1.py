4fed10bb7aae4a1093a94205adfd23276225f662
vladislav <vladislav.karnygin@gmail.com>
2021-11-11 10:26:38 +0300

    выполнил первое задания и скопировал их 111121

---
+++ Classwork/November/111121/ex1.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = 0
+c = a
+
+# условие на равенство и неравенство нулю
+if a == 0:
+    print("ValueError")
+else:
+    # цикл на добавление чисел
+    while a != 0:
+        a = int(input())
+        b += 1
+        c += a
+print(c / b)
