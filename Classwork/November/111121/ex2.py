d67cda9076a7c7d09789d11d29b83070f25e5ba2
vladislav <vladislav.karnygin@gmail.com>
2021-11-12 22:03:09 +0300

    выполнил второе задания 111121

---
+++ Classwork/November/111121/ex2.py
@@ -0,0 +1,16 @@
+summary = 0
+price = None
+
+# ввод цен и расчет суммы пока введенная цена не равна нулю
+while price != 0:
+    price = int(input("Вводите цену целым числом центов!"))
+    summary += price
+
+print("total price:", summary, "cents")
+
+# округление суммы для оплаты наличными
+summary = int(summary / 5) * 5 + (5 if (summary % 5) > 2.5 else 0)
+
+print("for payment by cash:", summary, "cents")
+
+
