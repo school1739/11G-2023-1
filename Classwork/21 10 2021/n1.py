110c13e203db9b261434d0115cf8479682ec6088
Bormotoon <bormotoon@gmail.com>
2021-11-13 14:08:34 +0300

    21.10.2021 evaluation. Интересно, кто же это за тебя делал. И почему не доделал. И почему КЛАССНАЯ РАБОТА от 21.10.21 закоммичена 11.11.21 в 10:10. Так много вопросов, так мало ответов...

--- Classwork/21 10 2021/n1.py
+++ Classwork/21 10 2021/n1.py
@@ -4,3 +4,5 @@ month = int(input())
 year = int(input())
 date = datetime.(year,month,day)
 next_date = date + datetime.timedelta(days=1)
+
+# Evaluation: SyntaxError: invalid syntax. >>>"Списал то, не знаю что".
