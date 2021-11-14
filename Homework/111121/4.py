2c95896793cf1b58cebb824493114f9a84f0cbb2
Kolodiy <agres321gmail.com>
2021-11-14 21:37:20 +0300

    Сделал №4 и удалил TODO(111121)

---
+++ Homework/111121/4.py
@@ -0,0 +1,15 @@
+price = 0
+while True:
+    # Ввод возраста
+    years = input("Введите возраст посетителя: ")
+    # Проверка на ввод пустого значения
+    if years == '' or years == ' ':
+        break
+    # Сопоставление возраста с ценой билета
+    elif 3 <= int(years) <= 12:
+        price += 150
+    elif 65 <= int(years):
+        price += 250
+    elif 12 < int(years) < 65:
+        price += 450
+print(f'Цена за билеты: {price}')
