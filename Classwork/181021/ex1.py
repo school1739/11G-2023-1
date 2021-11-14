b1abac42a789decb2eb8e22b9ed081e22d9750c5
Bormotoon <bormotoon@gmail.com>
2021-11-14 12:54:09 +0300

    18.10.2021 evaluation.

--- Classwork/181021/ex1.py
+++ Classwork/181021/ex1.py
@@ -3,3 +3,5 @@ if a % 2 == 0:
     print ("Чётное")
 else:
     print("Не чётное")
+
+# Evaluation: OK
--- Classwork/181021/ex2.py
+++ Classwork/181021/ex2.py
@@ -6,3 +6,5 @@ elif a <= 2:
     print(a * 10.5)
 else:
     print ((a - 2) * 4 + (10.5 * 2))
+
+# Evaluation: OK. Зачем дробная часть?
