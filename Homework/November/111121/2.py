fc280d6a95a6b6197fbe2855d36741f52715cc0e
Павел Бурыкин <pashabur18@gmail.com>
2021-11-15 15:07:33 +0300

    2(111121)

---
+++ Homework/November/111121/2.py
@@ -0,0 +1,7 @@
+print("Введите все цены в долларах:")
+prices = []
+while (price := input()) !='':
+    prices.append(float(price))
+summa = sum(prices)*100
+summa = round(summa/5)*5
+print(f'Округленная сумма:{summa/100}')
