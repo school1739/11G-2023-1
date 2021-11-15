606f4371550c0d498129333591f0f09da36bdfda
vladislav <vladislav.karnygin@gmail.com>
2021-11-15 15:51:44 +0300

    выполнил певрое задание 151121

---
+++ Classwork/November/151121/ex1.py
@@ -0,0 +1,8 @@
+# ввод пользователем числа
+maximum = int(input("введите число - "))
+
+# составляем таблицу умножения
+for row in range(1, maximum + 1):
+    for column in range(row, row * maximum + 1, row):
+        print("%4i" % column, end='\t')
+    print()
