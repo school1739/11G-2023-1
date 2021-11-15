bade3f3c3067a81333ba660a80ccc009fa1c06e0
vladislav <vladislav.karnygin@gmail.com>
2021-11-15 15:52:08 +0300

    выполнил второе задание 151121

---
+++ Classwork/November/151121/ex2.py
@@ -0,0 +1,15 @@
+n = int(input("enter the first number"))
+m = int(input("enter the second number"))
+
+d = 0
+
+# выражаем значение d
+if n >= m:
+    d = m
+else:
+    d = n
+
+# ищем наименьшее общее кратное
+while m % d != 0 or n % d != 0:
+    d = d - 1
+print("Least common multiple:", d)
