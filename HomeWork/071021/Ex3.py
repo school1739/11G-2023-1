3e994f7321a88ce96c364fcf4f7ab45c6fe1b13f
Enigma <killanite@mail.ru>
2021-11-17 17:40:24 +0300

    Выполнил задание 3 за 07.10

---
+++ HomeWork/071021/Ex3.py
@@ -0,0 +1,7 @@
+width=float(int(input("Ширина")))
+Length=float(int(input("Длина")))
+Height=float(int(input("Высота стен")))
+areaC=2*(width+Length)*Height
+areaK=(width+Length)*2
+print(areaC,"Площадь стен")
+print(areaK,"Площадь комнаты")
