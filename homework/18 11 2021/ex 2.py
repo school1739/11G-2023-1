9326c787314525cab7862dc94c281e5b420946de
glebs <glebstar28@gmail.com>
2021-11-18 09:58:52 +0300

    сделал 2 задание

---
+++ homework/18 11 2021/ex 2.py
@@ -0,0 +1,13 @@
+# Ввод чисел
+first_num = int(input("Введите положительное число: "))
+second_num = int(input("Введите положительное число: "))
+# Проверка на ввод неположитеьных чисел
+if first_num <= 0 or second_num <= 0:
+    print("Ошибка!")
+else:
+    # Выбор меньшего из чисел
+    divider = min(first_num, second_num)
+    # Проверка на наибольший общий делитель
+    while first_num % divider != 0 or second_num % divider != 0:
+        divider -= 1
+    print(divider)
