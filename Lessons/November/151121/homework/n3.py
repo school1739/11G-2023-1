f454764f84f1cf5b2023334393f5118105c156ac
kakashkaED <sergshupenkov@gmail.com>
2021-11-18 08:47:45 +0300

    номер 3

---
+++ Lessons/November/151121/homework/n3.py
@@ -0,0 +1,22 @@
+import random
+
+average_counter = 0
+for i in range(10):
+    counter = 0
+    # Список выпадения орлов и решек
+    lst = []
+    # Симуляция бросания монетки
+    while "ООО" not in lst and "РРР" not in lst:
+        coin = random.choice(['eagle', 'tails'])
+        # Проверка на выпадение орла или решки
+        if coin == 'eagle':
+            lst += "О"
+        else:
+            lst += "Р"
+        # Соединение элементов списка, чтобы цикл while смог их проверить
+        lst = "".join(lst)
+        # Счёт кол-ва попыток
+        counter += 1
+        average_counter += 1
+    print(f"{' '.join([str(i) for i in lst])} (Попыток: {counter})")
+print(f'Среднее количество попыток: {average_counter / 10}')
