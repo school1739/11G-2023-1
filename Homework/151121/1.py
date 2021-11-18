a8a6b6d196bdd30c5839014b2e3950bbf3a115e8
Spiriakov <vovus16@mail.ru>
2021-11-18 12:36:17 +0300

    Сделал 1 из 151121

---
+++ Homework/151121/1.py
@@ -0,0 +1,15 @@
+# Ввод мин. и макс. значений
+min = int(input("Введите мин. значение: "))
+max = int(input("Введите макс. значение: "))
+# Отступ
+print('', end="\t")
+# Первая строчка табл. умножения
+for i in range(min, max + 1):
+    print(i, end="\t")
+print()
+# Табл. умножения
+for i in range(min, max + 1):
+    print(i, end="\t")
+    for j in range(min, max + 1):
+        print(j * i, end="\t")
+    print()
