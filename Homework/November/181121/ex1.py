352ebab9df03b3b526278bb2fa8a49c4f06c7e1a
vladislav <vladislav.karnygin@gmail.com>
2021-11-19 18:54:25 +0300

    выполнил первое задание 181121

---
+++ Homework/November/181121/ex1.py
@@ -0,0 +1,13 @@
+entered_number = int(input("enter the number"))
+remnant = ''
+result = ''
+
+# цикл, определяющий остаток, результат от деления и число после деления на 2
+while entered_number > 0:
+    remnant = str(entered_number % 2)
+    print(entered_number, "/ 2 =", entered_number // 2, "remnant:", remnant)
+
+    result = remnant + result
+    entered_number = entered_number // 2
+
+print("finish = 0\nnumber in binary view:", result)
