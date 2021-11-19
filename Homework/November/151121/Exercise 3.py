a0c5b35c47c5aa1a1944b6e01a8e555123a03e63
khtimu05 <timur9709@gmail.com>
2021-11-19 11:15:08 +0300

    Задание 3

---
+++ Homework/November/151121/Exercise 3.py
@@ -0,0 +1,21 @@
+import random
+average_counter = 0
+for i in range(10):
+    counter = 0
+    # Список выпадения орлов и решек
+    lst = []
+    # Симуляция бросания монетки
+    while "ООО" not in lst and "РРР" not in lst:
+        coin = random.choice(['eagle', 'tails'])
+        # Проверка на выпадение орла или решки
+        if coin == 'eagle':
+            lst += "О"
+        else:
+            lst += "Р"
+        # Соединение элементов списка
+        lst = "".join(lst)
+        # Счётик попыток
+        counter += 1
+        average_counter += 1
+    print(f"{' '.join([str(i) for i in lst])} (Попыток: {counter})")
+print(f'Среднее количество попыток: {average_counter / 10}') #Вывод результата
