812df24817d522fe2735bef36a6813bcfcd73fcd
Enigma <killanite@mail.ru>
2021-11-20 12:23:30 +0300

    Выполнил задание 5 за 11.11.21 + перенёс задания

---
+++ HomeWork/111121/Ex5.py
@@ -0,0 +1,6 @@
+print("Группы из восьми бит:")
+while (bits := input()) != " ":
+    if len(bits) == 8 and all(c in ("0", "1") for c in bits):
+        print(int(bits.count("1") % 2))
+    else:
+        print("Error")
