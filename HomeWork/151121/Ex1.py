d95d627120b817bf893a60eb042913d86316a5c2
Enigma <killanite@mail.ru>
2021-11-20 16:56:24 +0300

    Выполнил задание 1 за 15.11.21

---
+++ HomeWork/151121/Ex1.py
@@ -0,0 +1,14 @@
+min = int(input("Мин. значение: "))
+max = int(input("Макс. значение: "))
+
+print('', end="\t")
+# Первая строчка табл. умножения
+for i in range(min, max + 1):
+    print(i, end="\t")
+print()
+# Табл. умножения
+for i in range(min, max + 1):
+    print(i, end="\t")
+    for g in range(min, max + 1):
+        print(g * i, end="\t")
+    print()
