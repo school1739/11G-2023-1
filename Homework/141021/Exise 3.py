eea595e04dcc97b426c0168a3d4b425d76868d9f
Егор <ryzhkove789@gmail.com>
2021-11-21 15:20:21 +0300

    Приступил к заданиям 21.10.21,(11 15 18)11.21

--- Homework/141021/Exise 3.py
+++
@@ -1,8 +0,0 @@
-print("Введите кол-во сувениров")
-n = int(input())
-print("Введите кол-во безделушек")
-z= int(input())
-print("Общая масса")
-print((n * 75))+(z*112)
-
-# Evaluation: TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
