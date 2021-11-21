55691017d432e70c7090e4d6c78f153c04f70458
kakashkaED <sergshupenkov@gmail.com>
2021-11-21 21:48:36 +0300

    номер 2

---
+++ Lessons/November/181121/homework/n2.py
@@ -0,0 +1,14 @@
+bin_num = int(input("Введите двоичное число: "))
+sum = ''
+sum2 = ''
+# Номер цифры с строке
+index = 0
+# Вывод метода расстановки степеней
+for i in range(len(str(bin_num)) - 1, -1, -1):
+    sum += f"{list(str(bin_num))[index]}∙2^{str(i)} + "
+    sum2 += f"{int(list(str(bin_num))[index]) * (2 ** i)} + "
+    index += 1
+print(f"{bin_num}(2) = {sum[0:-2]}= {sum2[0:-2]}= {int(str(bin_num), 2)}(10)"
+      f"\n"
+      f"\nОтвет: "
+      f"\n{bin_num}(2) = {int(str(bin_num), 2)}(10)")
---
+++ Lessons/November/181121/homework/n3.py
@@ -0,0 +1,17 @@
+import random
+changes = 0
+max_num = 0
+i=0
+while max_num!=100:
+    # Вывод рандомных чисел
+    num = random.randint(0,100)
+    # Поиск максимального числа. "i" нужно для того, чтобы первое число не считалось обновлением
+    if num> max_num and i!=0:
+        max_num=num
+        changes+=1
+        print(f"{num} <== Обновление")
+    else:
+        print(num)
+    i+=1
+print(f"Максимальное значение в ряду: 100 "
+      f"\nКоличество смен максимального значения: {changes}")
