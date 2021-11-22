1a720d0c3794ddd3f337d2c72ff24396bedd62f3
glebs <glebstar28@gmail.com>
2021-11-22 09:08:16 +0300

    сделал 1 задание

---
+++ homework/18 11 2021(2)/n1.py
@@ -0,0 +1,16 @@
+# Рекурсивная функция для перевода из десятичной системы счисления в двоичную
+def to_binary(decimal):
+    if decimal == 0:
+        return ''
+    # Находим частное и остаток от деления decimal на 2
+    quotient, mod = divmod(decimal, 2)
+    # Выводим их
+    print(f'{decimal} / 2 = {quotient}, остаток: {mod}')
+    # Продолжаем рекурсию
+    return to_binary(quotient) + str(mod)
+
+
+# Вводим число
+num = int(input('Введите число: '))
+# Выводим результат
+print(f'Число в двоичном представлении: {to_binary(num)}')
