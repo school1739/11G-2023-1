0fef7eac87b597cefb436faa26bdc90e21ea48e9
glebs <glebstar28@gmail.com>
2021-11-22 09:11:42 +0300

    сделал 3 задание

---
+++ homework/18 11 2021(2)/n3.py
@@ -0,0 +1,58 @@
+from math import sqrt
+from decimal import Decimal, getcontext
+
+
+# Формула Виета для приближения числа π
+def viet_formula(iters):
+    result = Decimal(1)
+    a = Decimal(0)
+    for _ in range(iters):
+        a = (2 + a).sqrt()
+        result *= a / 2
+    result = 2 / result
+    return result
+
+
+# Формула Валлиса
+def wallis_formula(iters):
+    result = Decimal(1)
+    for i in range(1, iters + 1):
+        a = Decimal(4 * (i ** 2))
+        result *= a / (a - 1)
+    result *= 2
+    return result
+
+
+# Ряд Лейбница
+def leibniz_formula(iters):
+    result = Decimal(0)
+    for i in range(iters):
+        result += (-1) ** i / Decimal(2 * i + 1)
+    result *= 4
+    return result
+
+
+# Составляем словарь с функциями формул и их номерами
+formulas = {
+    '1': viet_formula,
+    '2': wallis_formula,
+    '3': leibniz_formula
+}
+
+# Получаем номер формулы
+formula = input('''
+Выберите формулу (напишите номер)
+1. Формула Виета для приближения числа π
+2. Формула Валлиса
+3. Ряд Лейбница
+''')
+
+# Не знаю как производить вычисления до определённого знака после запятой, поэтому вводить надо количество итераций
+# и знаков после запятой при вычислениях
+iterations = int(input('Количество итераций: '))
+precision = int(input('Знаков после запятой при вычислениях: '))
+getcontext().prec = precision + 1
+# Берём из словаря функцию по введённому номеру и выводим результат
+print(formulas[formula](iterations))
+
+
