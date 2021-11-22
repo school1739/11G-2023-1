2a689eefc5cf544c9187625e8f1b3d47b3fce6f2
glebs <glebstar28@gmail.com>
2021-11-22 08:48:42 +0300

    сделал 1 задание

---
+++ homework/15 11 2021/n1.py
@@ -0,0 +1,7 @@
+for row in range(1, maximum + 1):# составляем таблицу умножения от единицы до десяти.
+    for column in range(row, row * maximum + 1, row):
+        print("%4i" % column, end='\t')
+    print()
+    # column(столбик)
+    # row(ряд)
+
