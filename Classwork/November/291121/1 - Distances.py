39696cb59179d1cb89445d744cd6f9db0a1b2822
vladislav <vladislav.karnygin@gmail.com>
2021-11-29 14:48:04 +0300

    выполнил первое задание 291121

--- Classwork/November/291121/1 - Distances.py
+++ Classwork/November/291121/1 - Distances.py
@@ -1,3 +1,5 @@
+import math
+
 # Есть словарь координат городов
 
 sites = {
@@ -11,6 +13,28 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+moscow = sites["Moscow"]
+london = sites["London"]
+paris = sites["Paris"]
 
-print(distances)
+# расчет расстояние между городами
+moscow_to_London = math.sqrt(((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2))
+london_to_paris = math.sqrt(((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2))
+paris_to_moscow = math.sqrt(((paris[0] - moscow[0]) ** 2 + (paris[1] - moscow[1]) ** 2))
+
+# добавления словарей в словарь
+distances["Moscow"] = {}
+distances["Moscow"]["London"] = str(moscow_to_London) + " km"
+distances["Moscow"]["Paris"] = str(paris_to_moscow) + " km"
+
+distances["Paris"] = {}
+distances["Paris"]["Moscow"] = str(paris_to_moscow) + " km"
+distances["Paris"]["London"] = str(london_to_paris) + " km"
+
+distances["London"] = {}
+distances["London"]["Moscow"] = str(moscow_to_London) + " km"
+distances["London"]["Paris"] = str(london_to_paris) + " km"
+
+# функция вывода словаря
+for i in distances:
+    print(i, distances[i])
