99443649a7d5eaa56994047d0c103a0101c97b76
vladislav <vladislav.karnygin@gmail.com>
2021-12-07 17:49:13 +0300

    выполнил третье задание 06/12/21

--- Classwork/December/061221/3 - Garden.py
+++ Classwork/December/061221/3 - Garden.py
@@ -17,17 +17,27 @@ for flower in all_the_flowers:
 
 # выведите на консоль те, которые растут и там и там
 print()
+
+# создаем лист цветов, которые растут и там и там
 itamitam = garden_set & meadow_set
+# печатаем каждый цветок по отдельности:
 for flower in itamitam:
     print(flower, end=" ")
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
 print()
+
+# создаем лист цветов, которые растут в саду, но не растут на лугу
 tamnonetam = garden_set - meadow_set
+# печатаем каждый цветок по отдельности:
 for flower in tamnonetam:
     print(flower, end=" ")
 
-
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 print()
+
+# создаем лист цветов, которые растут на лугу, но не растут в саду
 netamnotam = meadow_set - garden_set
+# печатаем каждый цветок по отдельности:
+for flower in netamnotam:
+    print(flower, end=" ")
