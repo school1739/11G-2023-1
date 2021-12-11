433c56e2854930db1ee586dba0bce1c6b086d235
Ramaz_Kui <pomazanov.timofei@gmail.com>
2021-12-11 12:07:37 +0300

    Сделал задание 4 на 29.11.2021

--- Lessons/November/291121/4 - Favorite movies.py
+++ Lessons/November/291121/4 - Favorite movies.py
@@ -12,4 +12,11 @@ my_favorite_movies = 'Терминатор, Пятый элемент, Ават�
 # Использовать .split() или .find() или другие методы строки нельзя - пользуйтесь только срезами,
 # как указано в задании!
 
-# TODO здесь ваш код
+a = my_favorite_movies[0] + my_favorite_movies[1] + my_favorite_movies[2] + my_favorite_movies[3] + my_favorite_movies[4] + my_favorite_movies[5] + my_favorite_movies[6] + my_favorite_movies[7] + my_favorite_movies[8] + my_favorite_movies[9]
+b = my_favorite_movies[42] + my_favorite_movies[43] + my_favorite_movies[44] + my_favorite_movies[45] + my_favorite_movies[46] + my_favorite_movies[47] + my_favorite_movies[48] + my_favorite_movies[49] + my_favorite_movies[50] + my_favorite_movies[51] + my_favorite_movies[52] + my_favorite_movies[53] + my_favorite_movies[54] + my_favorite_movies[55] + my_favorite_movies[56]
+c = my_favorite_movies[12] + my_favorite_movies[13] + my_favorite_movies[14] + my_favorite_movies[15] + my_favorite_movies[16] + my_favorite_movies[17] + my_favorite_movies[18] + my_favorite_movies[19] + my_favorite_movies[20] + my_favorite_movies[21] + my_favorite_movies[22] + my_favorite_movies[23] + my_favorite_movies[24]
+d = my_favorite_movies[35] + my_favorite_movies[36] + my_favorite_movies[37] + my_favorite_movies[38] + my_favorite_movies[39]
+print(a)
+print(b)
+print(c)
+print(d)
