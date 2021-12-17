75ef28249d70a8651cce76433963662e54187cb2
Bormotoon <bormotoon@gmail.com>
2021-12-17 13:25:57 +0300

    11.11.2021 evaluation: 2.

--- homework/11112021/n2.py
+++ homework/11112021/n2.py
@@ -10,3 +10,9 @@ while (price := input()) != '':
 summa = round(summa / 5) * 5
 # выводим сумму в долларах
 print(f'Округленная сумма: {summa/100}' )
+
+# Evaluation: NOT OK
+# Traceback (most recent call last):
+#   File "/python.21-22-1/homework/11112021/n2.py", line 10, in <module>
+#     summa = round(summa / 5) * 5
+# NameError: name 'summa' is not defined
