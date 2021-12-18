5084013d4f8d6ef9fa082213932108c134c3dc04
Bormotoon <bormotoon@gmail.com>
2021-12-18 08:53:06 +0300

    18.11.2021 evaluation: 5.

--- Homework/November/181121/1.py
+++ Homework/November/181121/1.py
@@ -1,8 +1,10 @@
-result  = ''
+result = ''
 q = int(input("Введите десятичное число: "))
 # Построение двоичной записи числа
-while q!=0:
-    result =str(q%2)+result
-    print(f"Остаток от деления: {q%2}\t Результат: {result}")
-    q = q//2
+while q != 0:
+    result = str(q % 2) + result
+    print(f"Остаток от деления: {q % 2}\t Результат: {result}")
+    q = q // 2
 print(f"Двоичное значение ввеленного числа: {result}")
+
+# Evaluation: OK. Забыл отформатировать перед коммитом.
--- Homework/November/181121/3.py
+++ Homework/November/181121/3.py
@@ -1,17 +1,20 @@
 import random
+
 changes = 0
 max_num = 0
-i=0
-while max_num!=100:
+i = 0
+while max_num != 100:
     # Вывод рандомных чисел
-    num = random.randint(0,100)
+    num = random.randint(0, 100)
     # Поиск максимального числа. "i" нужно для того, чтобы первое число не считалось обновлением
-    if num> max_num and i!=0:
-        max_num=num
-        changes+=1
+    if num > max_num and i != 0:
+        max_num = num
+        changes += 1
         print(f"{num} <== Обновление")
     else:
         print(num)
-    i+=1
+    i += 1
 print(f"Максимальное значение в ряду: 100 "
       f"\nКоличество смен максимального значения: {changes}")
+
+# Evaluation: OK
