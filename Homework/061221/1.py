ffd818ae8a0cab365df48b4037a1a438c3c90404
S1lence <shilin.egorka@yandex.ru>
2021-12-22 23:50:48 +0300

    выполнил все задания на 29.11 и 06.12

---
+++ Homework/061221/1.py
@@ -0,0 +1,26 @@
+
+# TODO здесь ваш код
+
+# Есть словарь песен группы Yellow со временем звучания с точностью до долей минут
+pocket_universe_songs = {
+    'Solar Driftwood': 1.85,
+    'Celsius': 5.98,
+    'More': 6.65,
+    'On Track': 5.55,
+    'Monolith': 6.35,
+    'To the Sea': 5.77,
+    'Magnetic': 5.88,
+    'Liquid Mountain': 2.97,
+    'Pan Blue': 5.52,
+    'Resistor': 7.22,
+    'Beyond Mirrors': 5.82,
+}
+
+# Распечатайте общее время звучания трех песен: 'On Track', 'To the Sea' и 'Beyond Mirrors'
+#   А другие три песни звучат приблизительно ХХХ минут
+
+sum = pocket_universe_songs['On Track'] + pocket_universe_songs['To the Sea'] + pocket_universe_songs['Beyond Mirrors']
+print('А другие три песни звучат приблизительно %.2f минут' % sum)
+
+# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
+# Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
