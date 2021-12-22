482984a6ae0298dc48a2e35da59cd21a29031f75
S1lence <shilin.egorka@yandex.ru>
2021-12-22 20:58:57 +0300

    Выполнил 1 29.11

---
+++ Homework/291121/1.py
@@ -0,0 +1,29 @@
+# Есть словарь координат городов
+
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
+
+# Составим словарь словарей расстояний между ними
+# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+distances = {}
+
+for k in sites.keys():
+    site = sites[k]
+    x1 = site[0]
+    y1 = site[1]
+    distances[k] = {}
+    k_keys = list(sites.keys())
+    k_keys.remove(k)
+    for n in k_keys:
+        nsite = sites[n]
+        x2 = nsite[0]
+        y2 = nsite[1]
+        distances[k][n] = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+for k in distances.keys():
+    for n in distances[k].keys():
+        print("%s - %s: %.2f" %(k, n, distances[k][n]))
