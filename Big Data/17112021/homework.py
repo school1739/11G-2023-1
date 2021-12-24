717b6c00d8921fa3b343e7bc8854d41fcd7babb4
Lisa Kulakova <kulakova.elizaveta0509@gmail.com>
2021-12-24 10:43:07 +0300

    Сделал задания по бд

---
+++ Big Data/17112021/homework.py
@@ -0,0 +1,20 @@
+# https://colab.research.google.com/drive/18HnEw0MM1yuASoQqZH4VMnYlF07ltLHf
+
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv('IGN_reviews.csv')
+dataframe = pd.DataFrame(dataset)
+
+dataframe
+
+dataframe.head()
+
+dataframe.tail()
+
+dataframe.info()
+
+dataframe.isnull().sum()
+
+new_dataframe = dataframe.dropna()
+new_dataframe.to_csv('reviews_non-nulled.csv')
