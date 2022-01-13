887f78e48c87efb2025db489e2d8e75f16408e82
vladislav <vladislav.karnygin@gmail.com>
2022-01-13 16:07:08 +0300

    fourth task completed

--- Python/January/130122/4 - Student.py
+++ Python/January/130122/4 - Student.py
@@ -7,6 +7,23 @@
 #       Расходы: ХХХ.ХХ рублей.
 #   Студенту надо попросить ХХХ.ХХ рублей
 
-educational_grant, expenses = 10000, 12000
+educational_grant, expenses = int(input("Стипендия - ")), int(input("Расходы - "))
 
-# TODO здесь ваш код
+
+months = ['September', 'October', 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June']
+
+
+for month in months:
+    if month == 'September':
+        cash_by_parents = expenses - educational_grant
+        print("September:")
+        print("Расходы:", expenses, "рублей")
+        print("Студенту надо попросить", cash_by_parents, "рублей")
+        print()
+    else:
+        cash_by_parents = round(expenses) - educational_grant
+        print(month + ":")
+        print("Расходы:", round(expenses), "рублей")
+        print("Студенту надо попросить", cash_by_parents, "рублей")
+        print()
+    expenses *= 1.03
