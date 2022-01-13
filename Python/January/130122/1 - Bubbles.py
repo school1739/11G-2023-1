d3a059489e5c7b463472a3b0ce48220e9a7fd6a3
LenovoUser <79779050037>
2022-01-13 10:48:34 +0300

    Классная работа

---
+++ Python/January/130122/1 - Bubbles.py
@@ -0,0 +1,22 @@
+import simple_draw as sd
+
+sd.set_screen_size(width=800, height=800)
+
+# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
+# bubble_center = sd.get_point(200, 200)
+# while True:
+#     sd.circle(bubble_center,60,color=(255, 255, 0))
+#     sd.circle(bubble_center,55,color=(255, 255, 0))
+#     sd.circle(bubble_center,50,color=(255, 255, 0))
+
+# Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
+# def buble_1():
+# Нарисовать 10 пузырьков в ряд
+# while True:
+#     for i in range(10):
+#         bubble_center = sd.get_point(i*100, 100)
+#         sd.circle(bubble_center,40)
+# Нарисовать три ряда по 10 пузырьков
+
+# Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
+sd.pause()
