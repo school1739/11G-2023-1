0bff2bc2061962511c811c8c0d4e94b5032d180d
khtimu05 <timur9709@gmail.com>
2022-01-16 19:53:16 +0300

    Envelop

--- Python/January/130122/2 - Envelop.py
+++ Python/January/130122/2 - Envelop.py
@@ -6,41 +6,75 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 # АВТОМАТИЧЕСКИ проверить для:
-# paper_x, paper_y = 8, 9
-# paper_x, paper_y = 9, 8
-# paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
-# paper_x, paper_y = 3, 4
-# paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
-# (написать цикл для проверки)
+# ввод размера
+envelop_x = int(input("Enter the width of paper"))
+envelop_y = int(input("Enter the length of paper"))
 
-# TODO здесь ваш код
+# размер листов
+size_of_paper = [
+    [8, 9],
+    [9, 8],
+    [6, 8],
+    [8, 6],
+    [3, 4],
+    [11, 9],
+    [9, 11]
+]
+
+# цикл проверки
+for papers in size_of_paper:
+    if envelop_x <= papers[0] and envelop_y <= papers[1]:
+        print("YES")
+    elif envelop_y <= papers[0] and envelop_x <= papers[1]:
+        print("YES")
+    else:
+        print("NO")
 
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# АВТОМАТИЧЕСКИ проверить для:
-# brick_x, brick_y, brick_z = 11, 10, 2
-# brick_x, brick_y, brick_z = 11, 2, 10
-# brick_x, brick_y, brick_z = 10, 11, 2
-# brick_x, brick_y, brick_z = 10, 2, 11
-# brick_x, brick_y, brick_z = 2, 10, 11
-# brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
-# (написать цикл для проверки)
+# Ввод размера отверстия
+hole_x = int(input("width_of_hole"))
+hole_y = int(input("length_of_hole"))
 
-# TODO здесь ваш код
+# Размеры кирпичей
+size_of_brick = [
+    [11, 10, 2],
+    [11, 2, 10],
+    [10, 11, 2],
+    [10, 2, 11],
+    [2, 10, 11],
+    [2, 11, 10],
+    [3, 5, 6],
+    [3, 6, 5],
+    [6, 3, 5],
+    [6, 5, 3],
+    [5, 6, 3],
+    [5, 3, 6],
+    [11, 3, 6],
+    [11, 6, 3],
+    [6, 11, 3],
+    [6, 3, 11],
+    [3, 6, 11],
+    [3, 11, 6],
+]
+
+# Проверка проходимости
+for bricks in size_of_brick:
+
+    if bricks[0] <= hole_x and bricks[1] <= hole_y:
+        print("YES")
+    elif bricks[1] <= hole_x and bricks[0] <= hole_y:
+        print("YES")
+    elif bricks[0] <= hole_x and bricks[2] <= hole_y:
+        print("YES")
+    elif bricks[2] <= hole_x and bricks[0] <= hole_y:
+        print("YES")
+    elif bricks[2] <= hole_x and bricks[1] <= hole_y:
+        print("YES")
+    elif bricks[1] <= hole_x and bricks[2] <= hole_y:
+        print("YES")
+    else:
+        print("NO")
