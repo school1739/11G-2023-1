c43cf92e455d26e27d5494c6383c175fe332a8b4
Kolodiy <agres321gmail.com>
2022-01-16 20:21:43 +0300

    Сделал №1 (130122)

--- Python/January/130122/1 - Bubbles.py
+++ Python/January/130122/1 - Bubbles.py
@@ -1,35 +1,39 @@
 import simple_draw as sd
 
-sd.set_screen_size(width=1400, height=700)
+sd.set_screen_size(width=1500, height=700)
 sd._init()
+
+
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
 
-bubble_center = sd.get_point(700, 500)
+def draw_bub(center, step, color):
+    for i in range(0, 15, 5):
+        sd.circle(center, step + i, color)
 
-sd.circle(bubble_center, 60)
-sd.circle(bubble_center, 55)
-sd.circle(bubble_center, 50)
+
+draw_bub(sd.Point(500, 100), 5, (255, 255, 0))
+
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 
-# def bubble(position,radius,color):
-#     sd.circle(position,radius,color)
+def bubble(center, step):
+    draw_bub(center, step, (255, 255, 0))
+
 
 # Нарисовать 10 пузырьков в ряд
 
-for i in range(100, 400, 30):
-    bubble_center = sd.get_point(i, 550)
-    sd.circle(bubble_center, 15)
+for i in range(10, 40, 3):
+    bubble_center = sd.get_point(i * 10, 550)
+    bubble(bubble_center, 5)
 
 # Нарисовать три ряда по 10 пузырьков
-for j in range(100, 400, 100):
-    for i in range(100, 400, 30):
-        bubble_center = sd.get_point(i, j)
-        sd.circle(bubble_center, 15)
+for j in range(1, 4, 1):
+    for i in range(10, 40, 3):
+        bubble_center = sd.Point(i * 10, j * 100)
+        bubble(bubble_center, 5)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 for i in range(100):
-    sd.circle(sd.random_point(), 30, sd.random_color())
-# Переделать
-# Пузырь это 3 круга, один в другом
+    draw_bub(sd.random_point(), 30, sd.random_color())
+
 sd.pause()
