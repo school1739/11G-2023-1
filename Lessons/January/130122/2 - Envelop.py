a84100a770dec742a2f2703ffea58a4b799b70e3
Enigma <killanite@mail.ru>
2022-01-20 16:44:00 +0300

    Сделал Envelop.

--- Lessons/January/130122/2 - Envelop.py
+++ Lessons/January/130122/2 - Envelop.py
@@ -15,7 +15,20 @@
 # paper_x, paper_y = 9, 11
 # (написать цикл для проверки)
 
-# TODO здесь ваш код
+envelop_size = (int(input('Ширина конверта: ')), int(input('Длина конверта: ')))
+paper_sizes = [
+    (8, 9), (9, 8), (6, 8),
+    (8, 6), (3, 4), (11, 9),
+    (9, 11)
+]
+
+for paper_size in paper_sizes:
+    if paper_size[0] <= envelop_size[0] and paper_size[1] <= envelop_size[1]:
+        print('Да')
+    else:
+        print('Нет')
+
+
 
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
@@ -43,4 +56,35 @@
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (написать цикл для проверки)
 
-# TODO здесь ваш код
+hole_x_y = int(input("Введите ширину отверстия:")),int(input("Введите длину отверстия:"))
+
+
+size_brick = [
+    [11, 10, 2],
+    [11, 2, 10],
+    [10, 11, 2],
+    [10, 2, 11],
+    [2, 10, 11],
+    [2, 11, 10],
+    [3, 5, 6],
+    [3, 6, 5],
+    [6, 3, 5],
+    [6, 5, 3],
+    [5, 6, 3],
+    [5, 3, 6],
+    [11, 3, 6],
+    [11, 6, 3],
+    [6, 11, 3],
+    [6, 3, 11],
+    [3, 6, 11],
+    [3, 11, 6],
+]
+for size in size_brick:
+    size.remove(max(size))
+    if size[0]<=hole_x_y[0] and size[1]<=hole_x_y[0]:
+        print("Да")
+    elif size[1]<=hole_x_y[0] and size[0]<=hole_x_y[0]:
+        print("Да")
+    else:
+        print("Нет")
+
