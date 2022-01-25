bd8f41c1c0394379bf380db6226e38cf3de12eda
vladislav <vladislav.karnygin@gmail.com>
2022-01-25 16:04:05 +0300

    copied tasks from main

--- Python/January/130122/1 - Bubbles.py
+++ Python/January/130122/1 - Bubbles.py
@@ -15,6 +15,7 @@ import random
 screen = sd.set_screen_size(width=1500, height=800)
 screen_init = sd._init()
 
+
 # функция создания пузыря
 def create_bubble(point, step, color):
     radius = 5
@@ -23,22 +24,21 @@ def create_bubble(point, step, color):
         sd.circle(center_position=point, radius=radius, color=color, width=1)
 
 
-#Создание ряда из 10 пузырей
+# Создание ряда из 10 пузырей
 for x in range(50, 550, 50):
     point = sd.get_point(x, 50)
-    create_bubble(point, step=3, color = sd.COLOR_WHITE)
-
+    create_bubble(point, step=3, color=sd.COLOR_WHITE)
 
 # Создание 3 рядов из 10 пузырей
-for y in range (100, 301, 100):
+for y in range(100, 301, 100):
     for x in range(50, 550, 50):
         point = sd.get_point(x, y)
-        create_bubble(point, step=3, color = sd.COLOR_BLACK)
+        create_bubble(point, step=3, color=sd.COLOR_BLACK)
 
 # #Создание 100 рандомных пузырей с рандомным цветом
-for y in range (10, 1000, 50):
+for y in range(10, 1000, 50):
     for x in range(10, 1000, 50):
-        point = sd.get_point(sd.random_number(10,1400), sd.random_number(10,800))
+        point = sd.get_point(sd.random_number(10, 1400), sd.random_number(10, 800))
         create_bubble(point, step=3, color=sd.random_color())
 
 sd.pause()
