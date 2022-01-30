a287095a38083c54f5d1b1b241c9d86ec4398324
kakashkaED <sergshupenkov@gmail.com>
2022-01-30 13:51:58 +0300

    n1

---
+++ classwork/240122/1-Hypotenuse.py
@@ -0,0 +1,20 @@
+"""Напишите функцию, принимающую на вход длины двух катетов прямо-
+угольного треугольника и возвращающую длину гипотенузы, рассчитан-
+ную по теореме Пифагора. В главной программе должен осуществляться
+запрос длин сторон у пользователя, вызов функции и вывод на экран
+полученного результата."""
+import math
+
+
+# подсчет гипотенузы
+def hypotenuse(first_cathetus, second_cathetus):
+    hypotenuse1 = math.sqrt(first_cathetus ** 2 + second_cathetus ** 2)
+    return hypotenuse1
+
+
+# вызов функции и округление значения гипотенузы
+length_of_hypotenuse = round(hypotenuse(first_cathetus=int(input("length of first cathetus: ")),
+                                        second_cathetus=int(input("length of second cathetus: "))), 2)
+
+# вывод длины гипотенузы
+print("length of hypotenuse is", length_of_hypotenuse)
