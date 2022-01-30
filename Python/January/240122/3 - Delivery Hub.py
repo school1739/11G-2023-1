78c9ce293cf43a10623315f82375d865e00150ff
Павел Бурыкин <pashabur18@gmail.com>
2022-01-30 16:57:28 +0300

    №3 на 240122

--- Python/January/240122/3 - Delivery Hub.py
+++ Python/January/240122/3 - Delivery Hub.py
@@ -5,3 +5,11 @@
 сумму доставки. В основной программе должны производиться запрос
 количества позиций в заказе у пользователя, и отображаться на экране
 сумма доставки (сумму перевести в рубли по курсу $1 = 75₽."""
+
+def delivery(number_product): # Ввод функции
+    payment = 10.95 * 70 + 2.95 * (number_product - 1) * 70
+    return payment
+
+x = round(delivery(number_product=int(input("number product:"))), 2)# Вызов функции
+
+print("payment:",x) #Цена
