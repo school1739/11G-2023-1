dc957bcbae14f99e421cf3e71b5f42d34466727d
Enigma <killanite@mail.ru>
2022-02-02 18:02:54 +0300

    Выполнил задание 3 за 24.01.22

--- Lessons/January/240122/3 - Delivery Hub.py
+++ Lessons/January/240122/3 - Delivery Hub.py
@@ -5,3 +5,11 @@
 сумму доставки. В основной программе должны производиться запрос
 количества позиций в заказе у пользователя, и отображаться на экране
 сумма доставки (сумму перевести в рубли по курсу $1 = 75₽."""
+
+def value_of_delivery(number_of_items):
+    payment_for_delivery = 10.95 * 75 + 2.95 * (number_of_items - 1) * 75
+    return payment_for_delivery
+
+round_of_value_delivery = round(value_of_delivery(number_of_items=int(input("enter the number of items - "))), 2)
+
+print("payment for delivery - ", round_of_value_delivery, "rubles")
