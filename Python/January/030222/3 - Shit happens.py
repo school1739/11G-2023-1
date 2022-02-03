b7c434457f6dbb1d4aede97f955a8701931b1316
LenovoUser <for.programming.77@gmail.com>
2022-02-03 12:36:22 +0300

    Начал задание 3

--- Python/January/030222/3 - Shit happens.py
+++ Python/January/030222/3 - Shit happens.py
@@ -1,11 +1,33 @@
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Под смайликом сделать подпись "Shit happens"
 
-# TODO здесь ваш код
+smile_radius = 200
 
-simple_draw.pause()
+
+def draw_face(x, y):
+    center = sd.get_point(x, y)
+    sd.circle(center, smile_radius, sd.COLOR_YELLOW, 0)
+
+    sd.polygon([
+        sd.get_point(center.x - smile_radius // 2, center.y - smile_radius // 10),
+        sd.get_point(center.x - smile_radius // 4, center.y - smile_radius // 2),
+        sd.get_point(center.x + smile_radius // 4, center.y - smile_radius // 2),
+        sd.get_point(center.x + smile_radius // 2, center.y - smile_radius // 10),
+    ], sd.COLOR_BLACK, 0)
+    sd.circle(center, smile_radius // 3, sd.COLOR_YELLOW, 0)
+
+    eyes_y = center.y + smile_radius // 2
+    eyes_shift_x = smile_radius // 2
+    eyes_radius = smile_radius // 6
+    sd.circle(sd.get_point(center.x - eyes_shift_x, eyes_y), eyes_radius, sd.COLOR_BLACK, 0)
+    sd.circle(sd.get_point(center.x + eyes_shift_x, eyes_y), eyes_radius, sd.COLOR_BLACK, 0)
+
+
+draw_face(300, 300)
+
+sd.pause()
