f7014e07b44fb286d8ffeaee60b564314fd4ebaf
Павел Бурыкин <pashabur18@gmail.com>
2022-02-03 12:46:21 +0300

    Перенёс 030222

---
+++ Python/February/030222/1 - Rainbow.py
@@ -0,0 +1,25 @@
+# (цикл for)
+
+import simple_draw as sd
+
+rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+
+# Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
+point = sd.get_point(700,-400)
+
+x = 800
+y = 0
+import time
+for x in range(800, 700, -8)
+    time.sleep(0.5)
+
+
+# Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
+
+
+# Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
+# поэкспериментировать с параметрами, что бы было красиво
+# TODO здесь ваш код
+
+sd.pause()
