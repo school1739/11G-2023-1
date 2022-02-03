8a3f4ebd9f533976933cfde7154ffe4cfbcf20fa
LenovoUser <>
2022-02-03 12:35:50 +0300

    All in all it's just another brick in the wall.

--- Python/February/030222/2 - Another brick in The Wall.py
+++ Python/February/030222/2 - Another brick in The Wall.py
@@ -1,10 +1,26 @@
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+brick_size = (100, 50)
+background_color = (100,) * 3
+space_width = 10
+rows = 500
+bricks_in_row = 100
+
+sd.rectangle(sd.get_point(0, 0), sd.get_point(*sd.resolution), background_color)
+for x in range(bricks_in_row):
+    for y in range(rows):
+        brick_x = (brick_size[0] + space_width) * x
+        brick_y = (brick_size[1] + space_width) * y
+        if y % 2 == 1:
+            brick_x -= (brick_size[0] + space_width) / 2
+        start = sd.get_point(brick_x, brick_y)
+        end = sd.get_point(brick_x + brick_size[0], brick_y + brick_size[1])
+        sd.rectangle(start, end, sd.COLOR_ORANGE)
+
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
@@ -15,4 +31,4 @@ import simple_draw
 #           вычисляем правый нижний и левый верхний углы кирпича
 #           рисуем кирпич
 
-simple_draw.pause()
+sd.pause()
