5e473e4c5acf54a04e49ca0f47481261c22bcb4f
Ramaz_Kui <pomazanov.timofei@gmail.com>
2022-02-05 22:01:42 +0300

    Почти сделал 3-е задание на 03.02.2022 (буквы всё ещё не ясно как сделать)

--- Python/Classwork/January/030222/3 - Shit happens.py
+++ Python/Classwork/January/030222/3 - Shit happens.py
@@ -6,14 +6,28 @@ import simple_draw as sd
 # Параметры функции: кордината X, координата Y, цвет.
 # Под смайликом сделать подпись "Shit happens"
 
-sd.background_color = (128, 128, 128)
+sd.background_color = (128, 128, 128)  # Серый фон
 sd.set_screen_size(width=1600, height=900)  # Разрешение моего экрана :(
 
-sd.circle(center_position=sd.get_point(800, 450), radius=200, color=sd.COLOR_DARK_YELLOW, width=0)
-sd.ellipse(left_bottom=sd.get_point(675, 300), right_top=sd.get_point(925, 450), color=sd.COLOR_BLACK, width=0)
-sd.ellipse(left_bottom=sd.get_point(675, 325), right_top=sd.get_point(925, 475), color=sd.COLOR_DARK_YELLOW, width=0)
-sd.ellipse(left_bottom=sd.get_point(700, 465), right_top=sd.get_point(750, 535), color=sd.COLOR_BLACK, width=0)
-sd.ellipse(left_bottom=sd.get_point(850, 465), right_top=sd.get_point(900, 535), color=sd.COLOR_BLACK, width=0)
 
+def facik(x, y):  # Делаем функцию (потому так написано в задании, а без неё всё пишется в 5 строчек)
+    # Главный шарик, сам смайлик
+    sd.circle(center_position=sd.get_point(x, y), radius=200, color=sd.COLOR_DARK_YELLOW, width=0)
+    # Улыбка (чёрный эллпис)
+    sd.ellipse(left_bottom=sd.get_point(x - 125, y - 150), right_top=sd.get_point(x + 125, y), color=sd.COLOR_BLACK,
+               width=0)
+    # Улыбка (жёлтый эллипс перекрывает чёрный, выходит улыбка)
+    sd.ellipse(left_bottom=sd.get_point(x - 125, y - 125), right_top=sd.get_point(x + 125, y + 25),
+               color=sd.COLOR_DARK_YELLOW,
+               width=0)
+    # Левый глаз
+    sd.ellipse(left_bottom=sd.get_point(x - 100, y + 15), right_top=sd.get_point(x - 50, y + 85), color=sd.COLOR_BLACK,
+               width=0)
+    # Правый глаз
+    sd.ellipse(left_bottom=sd.get_point(x + 50, y + 15), right_top=sd.get_point(x + 100, y + 85), color=sd.COLOR_BLACK,
+               width=0)
+
+
+facik(800, 450)
 
 sd.pause()
