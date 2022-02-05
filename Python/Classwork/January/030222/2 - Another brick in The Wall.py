a67cdd99906dafadf18c5ae383812316323fd7fb
Ramaz_Kui <pomazanov.timofei@gmail.com>
2022-02-05 20:55:21 +0300

    Я не понимаю, что не так в задании 2, я всё написал правильно

--- Python/Classwork/January/030222/2 - Another brick in The Wall.py
+++ Python/Classwork/January/030222/2 - Another brick in The Wall.py
@@ -3,20 +3,21 @@ import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-a1 = 0
-a2 = 50
-b1 = 100
-b2 = 155
 sd.background_color = (128, 128, 128)
-for i in range(10):
-    sd.rectangle(left_bottom=sd.get_point(a1, 0), right_top=sd.get_point(b1, 50), color=sd.COLOR_ORANGE, width=0)
-    a1 = a1 + 105
-    b1 = b1 + 105
-sd.rectangle(left_bottom=sd.get_point(0, 55), right_top=sd.get_point(45, 105), color=sd.COLOR_ORANGE, width=0)
-for i in range(10):
-    sd.rectangle(left_bottom=sd.get_point(a2, 55), right_top=sd.get_point(b2, 105), color=sd.COLOR_ORANGE, width=0)
-    a2 = a2 + 110
-    b2 = b2 + 110
+sd._to_screen(1600, 900)
+sd.set_screen_size(width=1600, height=900)
+x1 = -155
+y1 = -55
+x2 = -5
+y2 = -5
+for i in range(20):
+    y1 = y1 + 55
+    y2 = y2 + 55
+    for i in range(20):
+        x1 = x1 + 155
+        x2 = x2 + 155
+        sd.rectangle(left_bottom=sd.get_point(x1, y1), right_top=sd.get_point(x2, y2), color=sd.COLOR_ORANGE, width=0)
+
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
