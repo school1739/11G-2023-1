2d3e155e8af720567e281cacf80ca896a3ddaae6
timur <timokrat@yandex.ru>
2022-02-06 19:32:12 +0300

    Исправления

--- Python/January/030222/2 - Another brick in The Wall.py
+++ Python/January/030222/2 - Another brick in The Wall.py
@@ -1,5 +1,6 @@
 # (цикл for)
 import simple_draw as sd
+from random import randint
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
@@ -9,6 +10,7 @@ background_color = (100,) * 3
 space_width = 10
 rows = 10
 bricks_in_row = 10
+brick_color_difference = 30
 
 sd.rectangle(sd.get_point(0, 0), sd.get_point(*sd.resolution), background_color)
 for x in range(bricks_in_row):
@@ -17,9 +19,12 @@ for x in range(bricks_in_row):
         brick_y = (brick_size[1] + space_width) * y
         if y % 2 == 1:
             brick_x -= (brick_size[0] + space_width) / 2
-        start = sd.get_point(brick_x, brick_y)
-        end = sd.get_point(brick_x + brick_size[0], brick_y + brick_size[1])
-        sd.rectangle(start, end, sd.COLOR_ORANGE)
+        brick_start = sd.get_point(brick_x, brick_y)
+        brick_end = sd.get_point(brick_x + brick_size[0], brick_y + brick_size[1])
+        brick_color = (255 - randint(0, brick_color_difference),
+                       127 + randint(-brick_color_difference, brick_color_difference),
+                       randint(0, brick_color_difference))
+        sd.rectangle(brick_start, brick_end, brick_color)
 
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
