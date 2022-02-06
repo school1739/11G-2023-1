06aff65fbed2e732d0906625825029c71285b3bd
Kolodiy <agres321gmail.com>
2022-02-06 21:44:33 +0300

    Написал комментарии (310122) №3 пока без букв, скоро доделаю

--- Python/February/030222/1 - Rainbow.py
+++ Python/February/030222/1 - Rainbow.py
@@ -1,7 +1,8 @@
 # (цикл for)
 
 import simple_draw as sd
-sd.set_screen_size(1500,700)
+
+sd.set_screen_size(1500, 700)
 
 sd._init()
 
@@ -9,15 +10,16 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 
-
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 
-def raduga(clr,step):
-    sd.line(sd.get_point(50,50+step),sd.get_point(350,450+step),clr,4)
-k1=0
-for clr in rainbow_colors:
-    raduga(clr,k1)
-    k1-=5
+def raduga(clr, step):
+    sd.line(sd.get_point(50, 50 + step), sd.get_point(350, 450 + step), clr, 20)
+
+
+# Рисуем радугу
+for step1, clr in enumerate(rainbow_colors[::-1]):
+    raduga(clr, step1 * 20)
+
 
 # Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
 
@@ -25,12 +27,12 @@ for clr in rainbow_colors:
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 
-def raduga_circle(clr,step):
-    sd.circle(sd.get_point(750,0),500 - step,clr,15)
+def raduga_circle(clr, step):
+    sd.circle(sd.get_point(750, 0), 500 - step, clr, 15)
 
-k2=0
-for clr in rainbow_colors:
-    raduga_circle(clr,k2)
-    k2+=15
+
+# Рисуем радугу
+for step2, clr in enumerate(rainbow_colors):
+    raduga_circle(clr, step2 * 15)
 
 sd.pause()
