2cb144bf3d22a4d93e3e346ee119aa580f3d6b71
timur <timokrat@yandex.ru>
2022-02-07 22:01:12 +0300

    Продолжил делать игру

--- Python/The Game/main.py
+++ Python/The Game/main.py
@@ -1,65 +1,84 @@
-"""Написать программу, где две функции -- "Игрок 1" и "Игрок 2"
+"""
+Написать программу, где две функции -- "Игрок 1" и "Игрок 2"
 играют в игру, а третья функция -- "Судья" -- следит за ходом игры
 и ведёт счёт.
 
 Правила игры:
-Оба игрока кажый раунд выдают случайное целое число
+Оба игрока каждый раунд выдают случайное целое число
 в некотором диапазоне. Судья сравнивает эти числа и начисляет игрокам очки:
 Если числа равны, оба игрока получают 1 очко (+1). Когда число одно из игроков
 больше другого, игрок, который выдал большее число, получает 1 очко (+1),
 другой игрок штрафуется на 1 очко (-1). Игра продолжается до тех пор, пока
-один из игроков не наберёт 50 очков, но не более 100 раундов."""
+один из игроков не наберёт 50 очков, но не более 100 раундов.
+
+1. Основные правила (range: -1000, 1000)
+2. Режим "Читера": после 3 проигрышей увеличивается вероятность "чита".
+Чит: range(+1000, +1000). Изначальная вероятность считерить: 0%.
+Увеличение вероятности: +5%. После первой победы с читом чит отключается.
+Вероятность чита сохраняется.
+"""
 
 from random import randint
 
 points = [0] * 2
 ranges = [[-1000, 1000]] * 2
 defeats_in_row = [0] * 2
-cheat_chance = [0] * 2
+cheat_chances = [0] * 2
+
+max_rounds = 1000
+max_points = 50
 
 
 def player(player_num):
-    player_num -= 1
-
-    if defeats_in_row[player_num] >= 3:
-        cheat_chance[player_num] += 5
-        if cheat_chance[player_num] > 95:
-            cheat_chance[player_num] = 95
-        if cheat_chance[player_num] <= randint(0, 100):
-            ranges[player_num][0] += 1000
-            ranges[player_num][1] += 1000
-
+    try_cheat(player_num)
     return randint(*(ranges[player_num]))
 
 
 def try_cheat(player_num):
-    pass
+    if defeats_in_row[player_num] >= 3:
+        defeats_in_row[player_num] = 0
+        cheat_chances[player_num] += 5
+        if cheat_chances[player_num] > 95:
+            cheat_chances[player_num] = 95
+        if randint(0, 100) <= cheat_chances[player_num]:
+            ranges[player_num][0] += 1000
+            ranges[player_num][1] += 1000
+
+
+def player_win(player_num):
+    defeats_in_row[player_num] = 0
+    points[player_num] += 1
+
+
+def player_lost(player_num):
+    defeats_in_row[player_num] += 1
+    points[player_num] -= 1
 
 
 def judge(choice1, choice2):
     if choice1 == choice2:
-        points[0] += 1
-        points[1] += 1
+        player_win(0)
+        player_win(1)
     elif choice1 > choice2:
-        points[0] += 1
-        points[1] -= 1
-        defeats_in_row[1] += 1
+        player_win(0)
+        player_lost(1)
     else:
-        points[0] -= 1
-        points[1] += 1
-        defeats_in_row[0] += 1
+        player_lost(0)
+        player_win(1)
 
 
-for i in range(1000):
-    judge(player(1), player(2))
+for i in range(max_rounds):
+    judge(player(0), player(1))
 
-    if points[0] >= 50 and points[1] >= 50:
+    print(*ranges)
+
+    if points[0] >= max_points and points[1] >= max_points:
         print('Ничья!')
         break
-    elif points[0] >= 50:
+    elif points[0] >= max_points:
         print('1-ый игрок выиграл!')
         break
-    elif points[1] >= 50:
+    elif points[1] >= max_points:
         print('2-ой игрок выиграл!')
         break
 else:
