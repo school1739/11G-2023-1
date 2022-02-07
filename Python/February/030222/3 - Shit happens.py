ef8cc59afe75863763671c0053acb6af9df8e1a2
Computer <ryzhkove789@gmail.com>
2022-02-07 20:47:10 +0300

    Смайлик xD

--- Python/February/030222/3 - Shit happens.py
+++ Python/February/030222/3 - Shit happens.py
@@ -6,6 +6,24 @@ import simple_draw
 # Параметры функции: кордината X, координата Y, цвет.
 # Под смайликом сделать подпись "Shit happens"
 
-# .
+sd.resolution = (700, 700)
+
+
+def smile(x, y, color):
+    start_point = sd.get_point(x, y)
+    sd.circle(start_point, 50, color, width=4)
+    sd.line(sd.get_point(x - 20, y - 5), sd.get_point(x - 10, y - 20), color, 2)
+    sd.line(sd.get_point(x - 10, y - 20), sd.get_point(x + 10, y - 20), color, 2)
+    sd.line(sd.get_point(x + 10, y - 20), sd.get_point(x + 20, y - 5), color, 2)
+    sd.line(sd.get_point(x - 15, y + 20), sd.get_point(x - 15, y + 5), color, 2)
+    sd.line(sd.get_point(x + 15, y + 20), sd.get_point(x + 15, y + 5), color, 2)
+
+
+for i in range(10):
+    point = sd.random_point()
+    x = point.x
+    y = point.y
+    color = sd.random_color()
+    smile(x, y, color)
 
 simple_draw.pause()
