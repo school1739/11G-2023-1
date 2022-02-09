f733db1416cc378255d1771d01ce8401411e0278
Spiriakov <vovus16@mail.ru>
2022-02-09 22:26:50 +0300

    Читы вроде как работают, хотя и странно, минимальный ход 1647

--- Python/January/The Game/main.py
+++ Python/January/The Game/main.py
@@ -9,49 +9,70 @@
 больше другого, игрок, который выдал большее число, получает 1 очко (+1),
 другой игрок штрафуется на 1 очко (-1). Игра продолжается до тех пор, пока
 один из игроков не наберёт 50 очков, но не более 100 раундов."""
-import random
-
 from random import randint
 
-points1 = 0
-points2 = 0
+#Условия
 
+cheat_chances = [0] * 2
+points = [0] * 2
+defeats_in_row = [0] * 2
+ranges = [[-1000, 1000], [-1000, 1000]]
 
-def player1():
-    return randint(0, 10)
+max_rounds = 10000
+max_points = 50
 
+#Как работают эти ваши читы
 
-def player2():
-    return randint(0, 10)
+def player(player_num):
+   try_cheat(player_num)
+   return randint(*(ranges[player_num]))
 
+def try_cheat(player_num):
+     if defeats_in_row[player_num] >= 3:
+          defeats_in_row[player_num] = 0
+          cheat_chances[player_num] += 5
+          if cheat_chances[player_num] > 95:
+               cheat_chances[player_num] = 95
+          if randint(0, 100) <= cheat_chances[player_num]:
+               ranges[player_num][0] += 1000
+               ranges[player_num][1] += 1000
+
+#Как работают выигрыш и проигрыш
+
+def player_win(player_num):
+    defeats_in_row[player_num] = 0
+    points[player_num] += 1
+
+def player_lost(player_num):
+    defeats_in_row[player_num] += 1
+    points[player_num] -= 1
+
+#Делаем судью, расписал кто как и почему выигрывает
 
 def judge(choice1, choice2):
-    global points1, points2
     if choice1 == choice2:
-        points1 += 1
-        points2 += 1
+        player_win(0)
+        player_win(1)
     elif choice1 > choice2:
-        points1 += 1
-        points2 -= 1
+        player_win(0)
+        player_lost(1)
     else:
-        points1 -= 1
-        points2 += 1
+        player_lost(0)
+        player_win(1)
 
+#Как присуждают эти ваши очки
 
-rounds = 0
-while True:
-    judge(player1(), player2())
-    rounds += 1
-    if points1 >= 50 and points2 >= 50:
-        print('Победила дружба!')
-        break
-    elif points1 >= 50:
-        print('1-ый игрок молодец!')
-        break
-    elif points2 >= 50:
-        print('2-ой игрок молодец!')
-        break
-    rounds += 1
-    if rounds >= 1000000:
-        print('Раунды кончились!')
-        break
+for i in range(max_rounds):
+     judge(player(0), player(1))
+     print(f'Ход {i + 1}. Очки: {points}. Ranges: {ranges}')
+     if points[0] >= max_points and points[1] >= max_points:     #Выводим результат игры
+          print('Ничья!')
+          break
+     elif points[0] >= max_points:
+          print('1-ый игрок выиграл!')
+          break
+     elif points[1] >= max_points:
+          print('2-ой игрок выиграл!')
+          break
+     else:
+         print('Раунды кончились!')
