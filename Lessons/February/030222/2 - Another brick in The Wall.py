45121229b2b415d209daf9889b573d6046665f60
glebs <glebstar28@gmail.com>
2022-02-09 21:21:20 +0300

    январские долги и задания февраля

---
+++ Lessons/February/030222/2 - Another brick in The Wall.py
@@ -0,0 +1,36 @@
+
+
+# (цикл for)
+import simple_draw as sd
+
+# Нарисовать стену из кирпичей. Размер кирпича - 100х50
+# Использовать вложенные циклы for
+
+brick_size = (100, 50)
+background_color = (100,) * 3
+space_width = 10
+rows = 10
+bricks_in_row = 10
+
+sd.rectangle(sd.get_point(0, 0), sd.get_point(*sd.resolution), background_color)
+for x in range(bricks_in_row):
+    for y in range(rows):
+        brick_x = (brick_size[0] + space_width) * x
+        brick_y = (brick_size[1] + space_width) * y
+        if y % 2 == 1:
+            brick_x -= (brick_size[0] + space_width) / 2
+        start = sd.get_point(brick_x, brick_y)
+        end = sd.get_point(brick_x + brick_size[0], brick_y + brick_size[1])
+        sd.rectangle(start, end, sd.COLOR_ORANGE)
+
+# Подсказки:
+#  Для отрисовки кирпича использовать функцию rectangle
+#  Алгоритм должен получиться приблизительно такой:
+#
+#   цикл по координате Y
+#       вычисляем сдвиг ряда кирпичей
+#       цикл координате X
+#           вычисляем правый нижний и левый верхний углы кирпича
+#           рисуем кирпич
+
+sd.pause()
---
+++ Lessons/January/The Game/main.py
@@ -0,0 +1,53 @@
+"""Напишите функцию, принимающую на вход длины двух катетов прямо-
+угольного треугольника и возвращающую длину гипотенузы, рассчитан-
+ную по теореме Пифагора. В главной программе должен осуществляться
+запрос длин сторон у пользователя, вызов функции и вывод на экран
+полученного результата."""
+
+import random
+
+
+from random import randint
+
+points1 = 0
+points2 = 0
+
+
+def player1():
+    return randint(0, 10)
+
+
+def player2():
+    return randint(0, 10)
+
+
+def judge(choice1, choice2):
+    global points1, points2
+    if choice1 == choice2:
+        points1 += 1
+        points2 += 1
+    elif choice1 > choice2:
+        points1 += 1
+        points2 -= 1
+    else:
+        points1 -= 1
+        points2 += 1
+
+
+rounds = 0
+while True:
+    judge(player1(), player2())
+    rounds += 1
+    if points1 >= 50 and points2 >= 50:
+        print('Победила дружба!')
+        break
+    elif points1 >= 50:
+        print('1-ый игрок молодец!')
+        break
+    elif points2 >= 50:
+        print('2-ой игрок молодец!')
+        break
+    rounds+=1
+    if rounds >= 1000000:
+        print('Раунды кончились!')
+        break
