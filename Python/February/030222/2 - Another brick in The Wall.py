7eec5b86a2f3a4e886c87722aa84030b6eb4a0ce
Никита Неделько <ggarbe0@gmail.com>
2022-02-10 01:18:40 +0300

    2 задание сделанно (03.02.22)

--- Python/February/030222/2 - Another brick in The Wall.py
+++ Python/February/030222/2 - Another brick in The Wall.py
@@ -1,18 +1,16 @@
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
-# Подсказки:
-#  Для отрисовки кирпича использовать функцию rectangle
-#  Алгоритм должен получиться приблизительно такой:
-#
-#   цикл по координате Y
-#       вычисляем сдвиг ряда кирпичей
-#       цикл координате X
-#           вычисляем правый нижний и левый верхний углы кирпича
-#           рисуем кирпич
-
-simple_draw.pause()
+step = 0
+for y in range(0, 1000, 50):
+    y1 = y + 50
+    step -= 50
+    for x in range(step, 1000, 100):
+        x1 = x + 100
+        point = sd.get_point(x, y)
+        point1 = sd.get_point(x1, y1)
+        sd.rectangle(point, point1, color=sd.random_color(), width=0)
+sd.pause()
