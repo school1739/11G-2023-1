a00ce214a0d468041a801c62a10502a1617246c9
vladislav <vladislav.karnygin@gmail.com>
2022-02-12 14:10:23 +0300

    "The Game" was Updated, "Rainbow.py" completed, "Another brick in The Wall.py" completed

---
+++ Python/February/030222/1 - Rainbow.py
@@ -0,0 +1,37 @@
+# (цикл for)
+
+import simple_draw as sd
+
+rainbow_colors = (sd.COLOR_PURPLE, sd.COLOR_BLUE, sd.COLOR_CYAN, sd.COLOR_GREEN,
+                  sd.COLOR_YELLOW, sd.COLOR_ORANGE, sd.COLOR_RED)
+
+screen = sd.set_screen_size(width=1500, height=800)
+screen_init = sd._init()
+
+
+# Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
+def rainbow(x1, y1):
+    indent = 5
+    for i in range(7):
+        sd.line(sd.get_point(x1, y1 + i * indent), sd.get_point(x1 + 300, y1 + i * indent), width=4,
+                color=rainbow_colors[i])
+
+
+# Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
+
+
+# Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
+# поэкспериментировать с параметрами, что бы было красиво
+
+def rainbow_circle(radius, step, width):
+    point = sd.get_point(750, -200)
+    for color in rainbow_colors:
+        radius += step
+        sd.circle(center_position=point, radius=radius, color=color, width=width)
+
+
+# Calling teh Functions with given parameters
+rainbow_circle(500, 15, 15)
+rainbow(50, 600)
+
+sd.pause()
---
+++ Python/The Game/main.py
@@ -0,0 +1,99 @@
+"""Напишите функцию, принимающую на вход длины двух катетов прямо-
+угольного треугольника и возвращающую длину гипотенузы, рассчитан-
+ную по теореме Пифагора. В главной программе должен осуществляться
+запрос длин сторон у пользователя, вызов функции и вывод на экран
+полученного результата."""
+
+from random import randint
+
+# Specifying initial values
+number_of_rounds = 0
+player1_points = 0
+player2_points = 0
+
+SCORE = 'score'
+DEFEATS_IN_A_RAW = 'defeat_in_a_raw'
+CHEAT_PROB = 'cheat_prob'
+LAST_MOVE = 'last_move'
+
+players = [
+    {
+        SCORE: 0,
+        DEFEATS_IN_A_RAW: 0,
+        CHEAT_PROB: 0,
+        LAST_MOVE: None
+    },
+    {
+        SCORE: 0,
+        DEFEATS_IN_A_RAW: 0,
+        CHEAT_PROB: 0,
+        LAST_MOVE: None
+    }
+]
+
+
+# player move function
+def player(index):
+    players[index][LAST_MOVE] = randint(-1000, +1000) if randint(0, 99) > players[index][CHEAT_PROB] else randint(1000,
+                                                                                                                  1000)
+
+
+def referee(players):
+    if players[0][LAST_MOVE] == players[1][LAST_MOVE]:
+        print("Draw")
+        players[0][SCORE] += 1
+        players[1][SCORE] += 1
+        players[0][DEFEATS_IN_A_RAW] = players[1][DEFEATS_IN_A_RAW] = 0
+        return None
+
+    if players[0][LAST_MOVE] < players[1][LAST_MOVE]:
+        won_index, lose_index = 1, 0
+    else:
+        won_index, lose_index = 0, 1
+
+    players[won_index][SCORE] += 1
+    players[won_index][DEFEATS_IN_A_RAW] = 0
+    players[won_index][CHEAT_PROB] = 0
+
+    players[lose_index][SCORE] -= 1
+    players[lose_index][DEFEATS_IN_A_RAW] += 1
+    players[lose_index][CHEAT_PROB] += 5 if players[lose_index][DEFEATS_IN_A_RAW] % 3 == 0 else 0
+
+    return won_index + 1
+
+
+exit_flag = False
+
+# Process of The Game
+while not exit_flag:
+    number_of_rounds += 1
+
+    player(0)
+    player(1)
+
+    print("Move: player1={}, player2={}".format(players[0][LAST_MOVE], players[1][LAST_MOVE]))
+
+    # Calling the Function
+    won_player = referee(players)
+
+    print("Round {} stats: player1={}, player2={}".format(number_of_rounds, players[0], players[1]))
+
+    # if the the rounds are over
+    if number_of_rounds > 99:
+        print()
+        print("The rounds are over.")
+        if players[0][SCORE] == players[1][SCORE]:
+            print("Draw!")
+        elif players[0][SCORE] > players[1][SCORE]:
+            print("Player1 finally won!")
+        else:
+            print("Player2 finally won!")
+        exit_flag = True
+
+    if players[0][SCORE] == 50:
+        print("Player1 finally won!")
+        exit_flag = True
+
+    if players[1][SCORE] == 50:
+        print("Player2 finally won!")
+        exit_flag = True
