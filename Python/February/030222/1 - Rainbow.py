f8cb3cec640a324d639bfa75cb88b554dd4b4bde
whereismyapples <zalev135@gmail.com>
2022-02-13 19:51:11 +0300

    #1 радуги

--- Python/February/030222/1 - Rainbow.py
+++ Python/February/030222/1 - Rainbow.py
@@ -22,22 +22,20 @@ for i in range(7):
     sd.line(start_point=sd.get_point(start_x, start_y), end_point=sd.get_point(end_x, end_y), color=rainbow_colors[i],
             width=4)
 
-sd.pause()
-# Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
-start_x = 50
-start_y = 50
-end_x = 350
-end_y = 450
-step_x = 5
-step_y = 5
 
-for i in range(7):
-    start_x += i * step_x
-    start_y += i * step_y
-    end_x += i * step_x
-    end_y += i * step_y
-    sd.circle(start_point=sd.get_point(start_x, start_y), end_point=sd.get_point(end_x, end_y), color=rainbow_colors[i],
-            width=4)
+
+start_x = 700
+start_y = -50
+
+for current_collors in rainbow_colors:
+    center_position = sd.get_point(start_x, start_y)
+    sd.circle(center_position=center_position, radius=400, color=current_collors, width=15)
+    start_y -= 15
+
+sd.pause()
+
+# Подсказка: цикл нужно делать сразу по кортежу с цветами радуги.
+
 
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
