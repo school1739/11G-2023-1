a3a747a55388ae91d44a7cae80ca7d514d2d3b54
vladislav <vladislav.karnygin@gmail.com>
2022-02-14 14:44:32 +0300

    24.py completed and other tasks was copy path

---
+++ Python/February/140222/24.py
@@ -0,0 +1,21 @@
+s = open('24-1.txt').read()
+
+max_count = 0
+count = 1
+last_symbol = ''
+currently_symbol = ''
+
+for i in range(len(s)):
+    last_symbol = s[i-1]
+    currently_symbol = s[i]
+    if last_symbol != currently_symbol:
+        count += 1
+    else:
+        if max_count <= count:
+            max_count = count
+            count = 1
+        else:
+            count = 1
+
+    print('count={}, max_count={}'.format(count, max_count))
+print(max_count)
