975975c4d699e3059fdbd06c40bc92b951249bde
khtimu05 <timur9709@gmail.com>
2022-03-07 20:12:24 +0300

    24

---
+++ Python/February/140222/24.py
@@ -0,0 +1,15 @@
+
+result = 0
+with open('24-1.txt') as file:  # Читаем строки в файле
+    for string in file:
+        # Счётчик букв "A" и "E"
+        countA, countE = 0, 0
+        for char in string:
+            if char == 'A':
+                countA += 1
+            elif char == 'E':
+                countE += 1
+        if countA > countE:
+            result += 1
+
+print(result) #Вывод результата
