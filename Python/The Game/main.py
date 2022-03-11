79df62a42cf721d461f4c3e040d493e0207bdfd9
Никита Неделько <ggarbe0@gmail.com>
2022-03-11 03:44:15 +0300

    корект игры

--- Python/The Game/main.py
+++ Python/The Game/main.py
@@ -18,52 +18,68 @@
 
 from random import randint
 
-# начальные значения
-number_of_rounds = 0
-player1_points = 0
-player2_points = 0
+points = [0] * 2
+ranges = [[-1000, 1000], [-1000, 1000]]
+defeats_in_row = [0] * 2
+cheat_chances = [0] * 2
 
-# игрок 1
-def First_Player():
-    return randint(0, 150)
+max_rounds = 10000
+max_points = 50
 
-# игрок 2
-def Second_Player():
-    return randint(0, 150)
-# Рефери
-def Referee(first_peek, second_peek):
-    global player1_points, player2_points
 
-    if first_peek == second_peek:
-        player1_points += 1
-        player2_points += 1
-    elif first_peek < second_peek:
-        player1_points -= 1
-        player2_points += 1
+def player(player_num):
+    try_cheat(player_num)
+    return randint(*(ranges[player_num]))
+
+
+def try_cheat(player_num):
+    if defeats_in_row[player_num] >= 3:
+        defeats_in_row[player_num] = 0
+        cheat_chances[player_num] += 5
+        if cheat_chances[player_num] > 95:
+            cheat_chances[player_num] = 95
+        if randint(0, 100) <= cheat_chances[player_num]:
+            ranges[player_num][0] += 1000
+            ranges[player_num][1] += 1000
+
+
+def player_win(player_num):
+    defeats_in_row[player_num] = 0
+    points[player_num] += 1
+
+
+def player_lost(player_num):
+    defeats_in_row[player_num] += 1
+    points[player_num] -= 1
+
+
+def judge(choice1, choice2):
+    if choice1 == choice2:
+        player_win(0)
+        player_win(1)
+    elif choice1 > choice2:
+        player_win(0)
+        player_lost(1)
     else:
-        player1_points += 1
-        player2_points -= 1
-while True:
-    # функция
-    Referee(First_Player(), Second_Player())
+        player_lost(0)
+        player_win(1)
 
-    # исход если закончаться раунды
-    number_of_rounds += 1
-    if number_of_rounds > 100:
-        print('Раунды закончились')
-        exit()
 
-    # Результат игры
-    if player1_points == 50 and player2_points < 50:
-        print('Первый игрок победил')
-        exit()
+for i in range(max_rounds):
+    judge(player(0), player(1))
 
-    elif player2_points == 50 and player1_points < 50:
-        print('Второй игрок победил')
-        exit()
+    print(f'Ход {i + 1}. Очки: {points}. Ranges: {ranges}')
 
-    elif player1_points == 50 and player2_points == 50:
-        print('Ничья, идем по домам')
-        exit()
+    if points[0] >= max_points and points[1] >= max_points:
+        print('Ничья!')
+        break
+    elif points[0] >= max_points:
+        print('1-ый игрок выиграл!')
+        break
+    elif points[1] >= max_points:
+        print('2-ой игрок выиграл!')
+        break
+else:
+    print('Раунды кончились!')
 
 # NOT OK. При каждом запуске: Раунды закончились
