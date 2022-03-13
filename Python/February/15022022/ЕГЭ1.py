b9875e9ecbe032c51bce899cf44ff0bea73ba57a
Spiriakov <vovus16@mail.ru>
2022-03-13 23:10:16 +0300

    schetala s pravkoi

--- Python/February/15022022/ЕГЭ1.py
+++ Python/February/15022022/ЕГЭ1.py
@@ -2,14 +2,23 @@
 Текстовый файл состоит не более чем из 106 символов X, Y и Z. Определите длину самой длинной последовательности, состоящей из символов Y. Хотя бы один символ Y находится в последовательности.
 Для выполнения этого задания следует написать программу. Ниже приведён файл, который необходимо обработать с помощью данного алгоритма.
 '''
-s = open("24-1.txt").readline()
-k = 0
-l = 1
-for i in range(1, len(s)):
-    if s[i] == 'Y' and s[i - 1] == s[i]:
-        l+=1
-        k = max(k, l)
+s = open('24-1.txt').read()
+
+last_symbol = ''
+currently_symbol = ''
+schetala = 1
+maximal_schetala = 0
+
+for i in range(len(s)):
+    last_symbol = s[i-1]
+    currently_symbol = s[i]
+    if last_symbol != currently_symbol:
+        schetala += 1
     else:
-        l = 1
-print(k)
+        if maximal_schetala <= schetala:
+            maximal_schetala = schetala
+        count = 1
+
+    print('schetala={}, maximal_schetala={}'.format(schetala, maximal_schetala))
+print(maximal_schetala)
 
