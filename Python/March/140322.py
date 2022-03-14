273e9762bedf2a6d68d49fa932a38581c5ad6500
Student <>
2022-03-14 14:43:08 +0300

    14032022

---
+++ Python/March/140322.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self):
+        print("Конструктор создал игрока")
+        self.name = name
+        self.age = random.randint(12, 55)
+    def say(self, message):
+        print(Message)
+    def sayShit(self):
+        self.say("Shit")
+Ivanov = Player()
+Petrov = Player()
+print(Ivanov)
+Ivanov.say("I'm Ivanoff.")
+Petrov.say("I'm not Ivanoff. Fuck off.")
+Ivanov.sayShit()
+print(Ivanov.name)
+
+Ivanov.work="School"
+ 0.
