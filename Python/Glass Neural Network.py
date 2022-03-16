cea7312d3d3737d2ac87b9d9f1e68e4ceb369df2
timur <timokrat@yandex.ru>
2022-03-16 21:05:30 +0300

    Продолжил делать нейросеть

--- Python/Glass Neural Network.py
+++ Python/Glass Neural Network.py
@@ -1,6 +1,6 @@
 # Нейросеть из стаканов
 
-#Правила игры
+# Правила игры
 """
 На столе 11 палочек, два игрока ходят по очереди. За каждый ход можно взять 1 или 2 палочки.
 Проиграл тот, кто взял последнюю палочку.
@@ -29,4 +29,126 @@
 содержимое стаканов -- сколько и каких бумажек в них содержится, какова вероятность принятия того или
 иного решения в каждом "нейроне".
 
-Использование сторонних библиотек (pybrain, pygame, numpy и т.п.) запрещено. Можно использовать только random."""
+Использование сторонних библиотек (pybrain, pygame, numpy и т.п.) запрещено. Можно использовать только random.
+"""
+
+import random
+
+
+# Класс нейрона (стаканчика)
+class Neuron:
+    # Положить бумажки в стаканчик
+    def __init__(self):
+        self.values = [1] * 5 + [2] * 5
+        self.last_value = 0
+        self.fails_in_row = 0
+
+    # Взять случайную бумажку
+    def choose(self):
+        self.last_value = random.choice(self.values)
+        return self.last_value
+
+    # Убрать последнюю выбранную бумажку при проигрыше, если их больше одной
+    def fail(self):
+        if self.last_value != 0:
+            if len(self.values) > 1:
+                self.values.remove(self.last_value)
+            else:
+                self.fails_in_row += 1
+        self.last_value = 0
+
+    # Добавить новую бумажку с тем же значением, что и на выбранной
+    def success(self):
+        if self.last_value != 0:
+            self.values.append(self.last_value)
+            self.fails_in_row = 0
+        self.last_value = 0
+
+    # Ресетнуть нейрон, если всё плохо
+    def reset(self):
+        self.__init__()
+
+
+# Класс "искусственного интеллекта"
+class AI:
+    # Подготовить стаканчики
+    def __init__(self):
+        self.neurons = [Neuron() for _ in range(10)]
+
+    # Выбрать число палочек
+    def choose_number(self, sticks_left):
+        return self.neurons[sticks_left - 2].choose()
+
+    # Нейросеть проиграла
+    def fail(self):
+        for neuron in self.neurons:
+            neuron.fail()
+            if neuron.fails_in_row >= 10:
+                neuron.reset()
+
+    # Нейросеть выиграла
+    def win(self):
+        for neuron in self.neurons:
+            neuron.success()
+
+
+def player_turn():
+    return int(input('Сколько возьмёте палочек: 1 или 2?: '))
+
+
+def bot_turn(ai, sticks_left):
+    choice = ai.choose_number(sticks_left)
+    print(f'Я возьму столько палочек: {choice}')
+    return choice
+
+
+def random_turn():
+    choice = random.randint(1, 2)
+    print(f'Рандомно выбрал число: {choice}')
+    return choice
+
+
+# Создаём объект "искусственного интеллекта"
+bot = AI()
+
+# Количество рандомных раундов
+random_play_rounds = int(input('Сколько раундов сыграть рандомно? (Можно 0): '))
+
+# Цикл раундов
+while True:
+    # Оставшиеся палочки
+    sticks_left = 11
+    # 0 - ход игрока, 1 - нейросети
+    turn = random.randint(0, 1)
+    # Если надо, сыграть рандомно
+    random_play = False
+    if random_play_rounds > 0:
+        random_play = True
+        random_play_rounds -= 1
+    # Цикл игры
+    while sticks_left > 1:
+        print(f'Палочек осталось: {sticks_left}')
+        if turn == 0:
+            if random_play:
+                sticks_left -= random_turn()
+            else:
+                sticks_left -= player_turn()
+        else:
+            sticks_left -= bot_turn(bot, sticks_left)
+        turn = 1 - turn
+    print('Осталась последняя палочка!')
+    # Проверяем, кто проиграл
+    if turn == 0:
+        print('Ха-ха-ха! Я умнее тебя! У меня памяти 16 мегабайт!')
+        bot.win()
+    else:
+        print('Ты победил :(')
+        bot.fail()
+    # Выводим состояние нейросети
+    print('\nСтаканчики:')
+    print('1: Не используется')
+    for i, neuron in enumerate(bot.neurons):
+        print(f'{i + 2}: Бумажки: {neuron.values}')
+    print()
+
+# Не знаю, работает ли ресет стаканчика если в нём 1 бумажка и он 10 раз проиграл, так как такого не было :/
