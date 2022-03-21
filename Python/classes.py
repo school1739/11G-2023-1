2c3a45c8c7294c49ea6bfadeaf89aafe85708f58
Student <sdfasfwea@gmail.com>
2022-03-21 14:03:53 +0300

    Скачал classes.py

---
+++ Python/classes.py
@@ -0,0 +1,25 @@
+import random
+
+
+def globalSay(msg):
+    print(msg)
+
+
+class Player:
+    def __init__(self, name):
+        print("Конструктор создал игрока")
+        self.name = name
+        self.age = random.randint(12, 55)
+
+    def say(self, message):
+        print(message)
+
+    def sayShit(self):
+        self.say("Shit!")
+
+    def sayGlobal(self):
+        globalSay("Shit global")
+
+
+Ivanov = Player("Ivanoff")
+Petrov = Player("P.E. Trove")
