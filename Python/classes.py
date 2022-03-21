00bafb993d899bdfa553c5593708b140085705cf
Bormotoon <bormotoon@gmail.com>
2022-03-21 14:43:26 +0300

    Classes 2nd lesson.

--- Python/classes.py
+++ Python/classes.py
@@ -5,6 +5,18 @@ def globalSay(msg):
     print(msg)
 
 
+class Player_inCapsule:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    def say_name(self):
+        return self.__name
+
+    def say_age(self):
+        return self.__age
+
+
 class Player:
     def __init__(self, name):
         print("Конструктор создал игрока")
@@ -21,5 +33,22 @@ class Player:
         globalSay("Shit global")
 
 
+class PlayerJr(Player):
+    def __init__(self, name):
+        print("Конструктор создал МИНИ игрока")
+        self.name=name
+        self.school="1739"
+
+    def study(self):
+        print(self.name, "ботает в", self.school)
+
+
 Ivanov = Player("Ivanoff")
 Petrov = Player("P.E. Trove")
+Sidorov = Player_inCapsule("Sidor OFF", 55)
+
+IvanovJr = PlayerJr("Ivanoff Junior")
+print(IvanovJr.name)
+
+IvanovJr.study()
+IvanovJr.sayShit()
