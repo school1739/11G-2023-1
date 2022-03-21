f9c3ae2501219e1887ecac6acbe518d454e27d1c
Bormotoon <bormotoon@gmail.com>
2022-03-21 14:03:08 +0300

    Classes 2nd lesson.

--- Python/classes.py
+++ Python/classes.py
@@ -5,19 +5,6 @@ def globalSay(msg):
     print(msg)
 
 
-class Player_inCapsule:
-    def __init__(self, name, age):
-        print("Создан инкапуслированный игрок")
-        self.__name = name
-        self.__age = age
-
-    def say_name(self):
-        return self.__name
-
-    def say_age(self):
-        return self.__age
-
-
 class Player:
     def __init__(self, name):
         print("Конструктор создал игрока")
@@ -36,18 +23,3 @@ class Player:
 
 Ivanov = Player("Ivanoff")
 Petrov = Player("P.E. Trove")
-
-print(f"Player name: {Ivanov.name}      Player age: {Ivanov.age}")
-print(f"Player name: {Petrov.name}      Player age: {Petrov.age}")
-
-Ivanov.name = "Was Ivanoff once"
-Ivanov.age = 78
-
-print()
-print(f"Player name: {Ivanov.name}      Player age: {Ivanov.age}")
-
-print()
-Semenoff = Player_inCapsule("SemiOne", 55)
-
-print(Semenoff.say_age())
-print(Semenoff.say_name())
