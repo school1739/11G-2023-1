67b235cec5f67e90cae5d9e058413056fbfeb6dc
LenovoUser <22>
2022-03-24 10:47:00 +0300

    classwork

--- Python/classes.py
+++ Python/classes.py
@@ -1,6 +1,6 @@
 import random
 
-
+"""
 def globalSay(msg):
     print(msg)
 
@@ -23,3 +23,27 @@ class Player:
 
 Ivanov = Player("Ivanoff")
 Petrov = Player("P.E. Trove")
+"""
+class Dude:
+    def __init__(self, name):
+        self.__name= name
+        self.__age=random.randit(0,90)
+
+    def get_name(self): # функция-геттер -Name
+        return self.__name
+
+    def set_name(self, name):
+        self.name=name
+
+    def get_age(self): # функция-геттер- age
+        return self.__age
+
+
+    def set_age(self, age):
+        if 0<age<110:
+            self.__age=age
+        else:
+            print("Are yuo fucked up, dude?")
+
+    def get_info(self):# инфогеттер
+        print(f"Name: {self.__name,}")
