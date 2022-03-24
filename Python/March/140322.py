94d01e9af82aba7dae0b7abb3e7abeb30c5e6de7
Student <>
2022-03-24 10:46:28 +0300

    Lesson 24.03

--- Python/March/140322.py
+++ Python/March/140322.py
@@ -1,4 +1,5 @@
-class Player:
+import random
+"""class Player:
     def __init__(self):
         print("Конструктор создал игрока")
         self.name = name
@@ -15,5 +16,28 @@ Petrov.say("I'm not Ivanoff. Fuck off.")
 Ivanov.sayShit()
 print(Ivanov.name)
 
-Ivanov.work="School"
- 0.
+Ivanov.work="School"""
+"""class Dude:
+    def__init(self, name):
+@property
+        self.__name=name
+        print(f"{self.__name})
+        self.__age=random.randint(0, 90)
+@name.setter
+def set_age(self, age): #Функция сеттер
+    if 0<age<90:
+        self.__age=age
+    else:
+        print("Are you fucked up, dude?")
+def show_age(self):
+    return self.__age
+def say_name(self):
+    return self.__name
+@property
+Rick= dude("Rick")
+Rick.show_all()
+def set_name
+Rick=get.info
+Rick.name="Dick"
+print(Rick.name)
+"""
