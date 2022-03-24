4576653e00eb39d906d0d2a7188a501b4a93b564
Bormotoon <bormotoon@gmail.com>
2022-03-24 10:50:58 +0300

    Classes 3rd lesson.

--- Python/classes.py
+++ Python/classes.py
@@ -1,9 +1,8 @@
 import random
 
-
+"""
 def globalSay(msg):
     print(msg)
-
 
 class Player_inCapsule:
     def __init__(self, name, age):
@@ -51,4 +50,66 @@ IvanovJr = PlayerJr("Ivanoff Junior")
 print(IvanovJr.name)
 
 IvanovJr.study()
-IvanovJr.sayShit()
+IvanovJr.sayShit()"""
+
+"""class Dude:
+    def __init__(self, name):
+        self.__name = name
+        print(f"{self.__name} is born")
+        self.__age = random.randint(0, 90)
+
+    def get_name(self):  # Функция-геттер - NAME
+        return self.__name
+
+    def set_name(self, name):  # Функция-сеттер - NAME
+        self.__name = name
+
+    def get_age(self):  # Функция-геттер - AGE
+        return self.__age
+
+    def set_age(self, age):  # Функция-сеттер - AGE
+        if 0 < age < 110:
+            self.__age = age
+        else:
+            print("Are you fucked up, dude?")
+
+    def get_info(self):  # Инфогеттер
+        print(f"Name: {self.__name}, Age: {self.__age}")"""
+
+
+class Dude:
+    def __init__(self, name):
+        self.__name = name
+        print(f"{self.__name} is born")
+        self.__age = random.randint(0, 90)
+
+    @property
+    def name(self):  # Функция-геттер - NAME
+        return self.__name
+
+    @name.setter
+    def name(self, name):  # Функция-сеттер - NAME
+        last_name=self.__name
+        self.__name = name
+        print(f"{last_name} is now {self.__name}")
+
+    @property
+    def age(self):  # Функция-геттер - AGE
+        return self.__age
+
+    @age.setter
+    def age(self, age):  # Функция-сеттер - AGE
+        if 0 < age < 110:
+            self.__age = age
+        else:
+            print("Are you fucked up, dude?")
+
+    def get_info(self):  # Инфогеттер
+        print(f"Name: {self.__name}, Age: {self.__age}")
+
+
+Rick = Dude("Rick")
+# Rick.get_info()
+print(Rick.name)
+Rick.name = "Dick"
+print(Rick.name)
