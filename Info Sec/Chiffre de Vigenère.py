9938fbb9911d5b8ac41650ea3fa0d0b21e4b07d7
Ramaz_Kui <pomazanov.timofei@gmail.com>
2022-04-03 22:27:17 +0300

    Ура, сделал бедный, плохой, хилый, но шифратор!

--- Info Sec/Chiffre de Vigenère.py
+++ Info Sec/Chiffre de Vigenère.py
@@ -17,21 +17,23 @@ language = int(input(
     "Введите цифру языка, на котором вы напишите слово | Enter number of language, on which you will write a word (RU - 1, EU - 2): "))
 
 if language == 1:
-    keywordRU = input("Введите ключевое слово: ")
-    wordRU = input("Введите слово, которое хотите зашифровать: ")
+    keywordRU = input("Введите ключевое слово (только заглавными буквами): ")
+    wordRU = input("Введите слово, которое хотите зашифровать (только заглавными буквами): ")
     keywordRU_spisok = []
     wordRU_spisok = []
-    a = len(wordRU)
-    for a in wordRU:
-        for b in keywordRU:
-            keywordRU_index = int(alphabet_RU.find(b) + 1)
-            keywordRU_spisok = keywordRU_spisok.append(keywordRU_index)
-        for c in wordRU:
-            wordRU_index = int(alphabet_RU.find(c) + 1)
-            wordRU_spisok = wordRU_spisok.append(wordRU_index)
+    for b in keywordRU:
+        keywordRU_index = int(alphabet_RU.find(b) + 1)
+        keywordRU_spisok.append(keywordRU_index)
+
+    for c in wordRU:
+        wordRU_index = int(alphabet_RU.find(c) + 1)
+        wordRU_spisok.append(wordRU_index)
+
     for i in range(len(wordRU_spisok)):
         sum = wordRU_spisok[i] + keywordRU_spisok[i % len(keywordRU_spisok)]
         print(alphabet_RU[sum], end="")
+
+
 
 elif language == 2:
     keyword2 = input("Enter keyword: ")
