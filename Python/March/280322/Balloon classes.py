1844a96510342d1f47b377deb302471d25967f08
Enigma <killanite@mail.ru>
2022-04-05 21:56:02 +0300

    Выполнил Balloon.

--- Python/March/280322/Balloon classes.py
+++ Python/March/280322/Balloon classes.py
@@ -17,16 +17,45 @@ import random
 инкапсуляция, функции и классовые методы - произвольно.
 Использование pygame и/или иных библиотек, кроме SD и random не допускается.
 """
+width = 1500
+height = 700
+sd.set_screen_size(width, height)
+sd._init()
 
-class Firework: # Основной класс фейерверка
-    def __init__(self):
-        pass
+center = sd.get_point(width / 2, height / 2)
 
-class FireworkCenter(Firework): # Подкласс для большой центральной части фейерверка
-    pass
 
-class FireworkAround(Firework): # Подкласс для маленьких фейерверков вокруг основного
-    pass
+class Firework:
+    def fireworks(x, y):
+        sd.circle(sd.get_point(x, y), 10, sd.random_color(), 0)
 
-class Balloon: # Класс для шарика
-    pass
+class FireworkCenter(Firework):
+    for i in range(1000):
+        Firework.fireworks(center.x + random.randint(-150, 150), center.y + random.randint(-150, 150))
+
+class FireworkAround(Firework):
+    for indent in range(3):
+        for i in range(300):
+            Firework.fireworks(center.x + random.randint(-450, -350) + 400 * indent,
+                               center.y + random.randint(250, 350))
+    for indent in range(1):
+        for i in range(300):
+            Firework.fireworks(center.x + random.randint(-250, -150) + 400 * indent,
+                               center.y + random.randint(-350, -250))
+
+class Balloon:
+    def balloon(point):
+        sd.ellipse(sd.get_point(point.x - 20, point.y - 30), sd.get_point(point.x + 20, point.y + 30),
+                   sd.random_color())
+
+        sd.polygon(point_list=[sd.get_point(point.x, point.y - 30), sd.get_point(point.x - 5, point.y - 40),
+                               sd.get_point(point.x + 5, point.y - 40)], color=sd.random_color(), width=0)
+
+        sd.line(sd.get_point(point.x, point.y - 40), sd.get_point(point.x - 5, point.y - 60))
+        sd.line(sd.get_point(point.x - 5, point.y - 60), sd.get_point(point.x + 5, point.y - 80))
+        sd.line(sd.get_point(point.x + 5, point.y - 80), sd.get_point(point.x - 5, point.y - 100))
+        sd.line(sd.get_point(point.x - 5, point.y - 100), sd.get_point(point.x + 5, point.y - 120))
+
+for i in range(15):
+    Balloon.balloon(sd.random_point())
+sd.pause()
