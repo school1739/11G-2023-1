3f2fb774c608861fec3a5d660f49f7ce27d00268
Kayutkin <glebstar28@gmail.com>
2022-04-07 10:46:23 +0300

    march

---
+++ Lessons/March/140322.py
@@ -0,0 +1,43 @@
+import random
+"""class Player:
+    def __init__(self):
+        print("Конструктор создал игрока")
+        self.name = name
+        self.age = random.randint(12, 55)
+    def say(self, message):
+        print(Message)
+    def sayShit(self):
+        self.say("Shit")
+Ivanov = Player()
+Petrov = Player()
+print(Ivanov)
+Ivanov.say("I'm Ivanoff.")
+Petrov.say("I'm not Ivanoff. Fuck off.")
+Ivanov.sayShit()
+print(Ivanov.name)
+
+Ivanov.work="School"""
+"""class Dude:
+    def__init(self, name):
+@property
+        self.__name=name
+        print(f"{self.__name})
+        self.__age=random.randint(0, 90)
+@name.setter
+def set_age(self, age): #Функция сеттер
+    if 0<age<90:
+        self.__age=age
+    else:
+        print("Are you fucked up, dude?")
+def show_age(self):
+    return self.__age
+def say_name(self):
+    return self.__name
+@property
+Rick= dude("Rick")
+Rick.show_all()
+def set_name
+Rick=get.info
+Rick.name="Dick"
+print(Rick.name)
+"""
