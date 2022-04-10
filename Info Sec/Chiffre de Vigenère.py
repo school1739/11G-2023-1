467f52468f80914e017979b76eb6205604bbf741
Никита Неделько <ggarbe0@gmail.com>
2022-04-10 04:03:03 +0300

    homework insosec

--- Info Sec/Chiffre de Vigenère.py
+++ Info Sec/Chiffre de Vigenère.py
@@ -12,7 +12,7 @@ alphabet_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 #       Ci=(Pi+Ki) mod 26
 #   Расшифровка:
 #       Pi=(Ci-Ki+26) mod 26
-
+"""
 plain_text=input("Plain: ")
 key_text=input("Key: ")
 for key_char in plain_text:
@@ -25,6 +25,29 @@ for plain_char in plain_text:
 
 for char in (plain_text):
     print(alphabet_RU[plaint_index+key_index])
+"""
+from itertools import cycle
+
+LEN = 127
 
 
+def full_encode(value, key):
+    return ''.join(map(lambda x: chr((ord(x[0]) + ord(x[1])) % LEN), zip(value, cycle(key))))
 
+
+def full_decode(value, key):
+    return ''.join(map(lambda x: chr((ord(x[0]) - ord(x[1]) + LEN) % LEN), zip(value, cycle(key))))
+
+
+if __name__ == "__main__":
+    word = input()
+    key = input()
+
+    print ('Слово: ' + word)
+    print ('Ключ: ' + key)
+
+    shifre = full_encode(word, key)
+    print ('Шифр=', shifre)
+
+    decoded = full_decode(shifre, key)
+    print ('Word=', decoded)
