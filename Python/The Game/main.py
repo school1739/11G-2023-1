a5526190589e2488f169174bfcf68e4cd11572d6
Student <lady.khodina@yandex.ru>
2022-04-25 14:36:03 +0300

    Я сделла The Game

--- Python/The Game/main.py
+++ Python/The Game/main.py
@@ -12,48 +12,111 @@
 
 from random import randint
 
-points1 = 0
-points2 = 0
+max_rounds = 10000
+max_points = 50
 
 
-def player1():
-    return randint(0, 10)
+# Класс игрока
+class Player:
+    # Конструктор класса
+    def __init__(self):
+        self.points = 0
+        self.range = [-1000, 1000]
+        self.defeats_in_row = 0
+        self.cheat_chance = 0
+
+    # Имитация хода игрока
+    def make_turn(self):
+        self.try_cheat()
+        return randint(*self.range)
+
+    # Попытка считерить, если проиграл 3 раза подряд
+    def try_cheat(self):
+        if self.defeats_in_row >= 3:
+            self.defeats_in_row = 0
+            self.cheat_chance += 5
+            if self.cheat_chance > 95:
+                self.cheat_chance = 95
+            if randint(0, 100) <= self.cheat_chance:
+                self.range[0] += 1000
+                self.range[1] += 1000
+
+    # Игрок победил
+    def win(self):
+        self.defeats_in_row = 0
+
+    # Ничья
+    def draw(self):
+        self.win()
+
+    # Игрок проиграл
+    def lose(self):
+        self.defeats_in_row += 1
 
 
-def player2():
-    return randint(0, 10)
+# Класс судьи
+class Judge:
+    # Конструктор класса
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+    # Сыграть раунд
+    def iteration(self):
+        choice1 = self.player1.make_turn()
+        choice2 = self.player2.make_turn()
+        if choice1 == choice2:
+            self.player1.draw()
+            self.player2.draw()
+            self.add_points(1, 1)
+        elif choice1 > choice2:
+            self.player1.win()
+            self.player2.lose()
+            self.add_points(1, -1)
+        else:
+            self.player1.lose()
+            self.player2.win()
+            self.add_points(-1, 1)
+
+        if self.player1.points >= max_points and self.player2.points >= max_points:
+            return 'Ничья!'
+        if self.player1.points >= max_points:
+            return '1-ый игрок выиграл!'
+        if self.player2.points >= max_points:
+            return '2-ой игрок выиграл!'
+        return None
+
+    # Если раунды закончились
+    def rounds_over(self):
+        if self.player1.points == max_points and self.player2.points >= max_points:
+            return 'Ничья!'
+        if self.player1.points > self.player2.points:
+            return '1-ый игрок выиграл!'
+        return '2-ой игрок выиграл!'
+
+    # Добавить очки игрокам
+    def add_points(self, p1, p2):
+        self.player1.points += p1
+        self.player2.points += p2
 
 
-def judge(choice1, choice2):
-    global points1, points2
-    if choice1 == choice2:
-        points1 += 1
-        points2 += 1
-    elif choice1 > choice2:
-        points1 += 1
-        points2 -= 1
-    else:
-        points1 -= 1
-        points2 += 1
+# Создаём игроков и судью
+player1 = Player()
+player2 = Player()
+judge = Judge(player1, player2)
 
+# Цикл игры
+for i in range(max_rounds):
+    result = judge.iteration()
 
-rounds = 0
-while True:
-    judge(player1(), player2())
+    print(f'Ход {i + 1}. Очки: {(player1.points, player2.points)}. Ranges: {(player1.range, player2.range)}')
 
-    if points1 >= 50 and points2 >= 50:
-        print('Победила бебра!')
+    if result is not None:
+        print(result)
         break
-    elif points1 >= 50:
-        print('1-ый игрок выиграл!')
-        break
-    elif points2 >= 50:
-        print('2-ой игрок выиграл!')
-        break
-
-    rounds += 1
-    if rounds >= 1000:
-        print('Раунды кончились!')
-        break
+# Раунды закончились
+else:
+    print('Раунды кончились!')
+    print(judge.rounds_over())
 
 # NOT OK. При каждом запуске "Раунды кончились!" и больше ничего.
