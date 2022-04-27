b5663f48eae678fe1adaaee506d77fc18f924c76
Kolodiy <agres321gmail.com>
2022-04-27 20:49:02 +0300

    Доделал красивый вывод

--- Python/The Game/main_2.py
+++ Python/The Game/main_2.py
@@ -1,7 +1,9 @@
 import random
+
 from rich.console import Console
 from rich.table import Table
 
+console = Console()
 # Некоторый диапазон
 range_numbers = [-1000, 1000]
 
@@ -13,7 +15,7 @@ def cheat(probabilty_cheat, amount_fails):
     # Вероятность срабатывания чита после 3 проигрышей подряд
     if probabilty <= probabilty_cheat and amount_fails >= 3:
         cheat = True
-        print("---Чит активирован---")
+        # print("---Чит активирован---")
     return cheat
 
 
@@ -37,25 +39,26 @@ class Gamer:
             self.probability_cheat += 5
             if self.probability_cheat == 100:  # Если вероятность чита равна 100%, то она обнуляется, т.к. иначе было бы не честно
                 self.probability_cheat = 0
-        is_cheat = False
+        is_cheat = "✖"
         if cheat(self.probability_cheat, fails_in_a_row) == True:  # Чит
             self.num += 1000
-            is_cheat = True
+            is_cheat = "✔"
         return self.num, is_cheat
 
 
 gamer_1 = Gamer("1")
 gamer_2 = Gamer("2")
 
+# Название таблицы и её столбцов
 table = Table(title="Game progress")
-table.add_column("Раунд")
-table.add_column("Число 1 игрока")
-table.add_column("Число 2 игрока")
-table.add_column("Счёт 1 игрока")
-table.add_column("Счёт 2 игрока")
-table.add_column("Чит 1?")
-table.add_column("Чит 2?")
-table.add_column("Победитель")
+table.add_column("Раунд", justify="center", style="blue")
+table.add_column("Число 1 игрока", justify="center")
+table.add_column("Число 2 игрока", justify="center")
+table.add_column("Счёт 1 игрока", justify="center")
+table.add_column("Счёт 2 игрока", justify="center")
+table.add_column("Чит 1?", justify="center", style="yellow")
+table.add_column("Чит 2?", justify="center", style="yellow")
+table.add_column("Победитель", justify="center", style="green", no_wrap=True)
 
 
 class Referee:
@@ -63,50 +66,47 @@ class Referee:
         print("Судья появился и готов следить за игрой")
 
     def Game(self, i):  # Игра
-        global fails1_in_a_row
+        global fails1_in_a_row, winner
         global fails2_in_a_row
         global score_1, score_2
         num_1 = gamer_1.number(fails1_in_a_row)
         num_2 = gamer_2.number(fails2_in_a_row)
-        print(num_1[1],num_2[1])
         if num_2[0] == num_1[0]:
             score_1 += 1
             score_2 += 1
             fails1_in_a_row, fails2_in_a_row = 0, 0
-            print("Ничья")
             winner = "Ничья"
         elif num_2[0] > num_1[0]:
             score_1 -= 1
             score_2 += 1
             fails1_in_a_row += 1
             fails2_in_a_row = 0
-            print("Второй выиграл")
-            winner = 2
+            winner = "Второй"
         elif num_2[0] < num_1[0]:
             score_1 += 1
             score_2 -= 1
             fails1_in_a_row = 0
             fails2_in_a_row += 1
-            print("Первый выиграл")
-            winner = 1
+            winner = "Первый"
 
-        table.add_row(str(i), str(num_1), str(num_2), str(score_1), str(score_2), str(num_1[1]), str(num_2[1]),str(winner))
-# Доделать
+        # Добавляем значения в столбцы
+        table.add_row(str(i + 1), str(num_1[0]), str(num_2[0]), str(score_1), str(score_2), str(num_1[1]),
+                      str(num_2[1]),
+                      str(winner))
+
 
 Referee = Referee()
 
 # Раунды
 for i in range(10000):
     Referee.Game(i)  # Раунд одной игры
-    print(f"Счёт 1 игрока:", score_1)
-    print(f"Счёт 2 игрока:", score_2)
     if score_1 == 50:
         print(f'Первый победил в игре')
         break
     elif score_2 == 50:
         print(f'Второй победил в игре')
         break
-    print()
+console.print(table)
 
 if score_1 < 50 and score_2 < 50:
     print(f'Никто не дошел до победого счета')
