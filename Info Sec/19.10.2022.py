5f8dbc2abeaf60347dcbadddbcec8e197ef2bcae
Ramaz_Kui <pomazanov.timofei@gmail.com>
2022-10-19 09:03:15 +0300

    Отправили с Лизой код для шифровки другой группе

--- Info Sec/19.10.2022.py
+++ Info Sec/19.10.2022.py
@@ -13,3 +13,20 @@ ftp.connect(HOST, PORT)
 print(f'Conecting sucess!\nLogin as: {USER},Pass: {PASSWORD}')
 ftp.login(USER, PASSWORD)
 print('Login Succes!')
+
+filename = "Decode.txt"
+
+# Read file in binary mode
+with open(filename, "rb") as file:
+    # Command for Uploading the file "STOR filename"
+    ftp.storbinary(f"STOR {filename}", file)
+
+#Vizhinere
+#YOU WILL DIE RIGHT NOW!
+#SCHOOL
+#RRC LUEO SXQ UQVWF QWLN
+
+#CESAR
+#We all will die
+#5
+#1j%fqq%Bnqq%inj
