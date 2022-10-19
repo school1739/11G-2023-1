fbcc5539e4bed4999016b13aee26e6da3d53ca83
Ramaz_Kui <pomazanov.timofei@gmail.com>
2022-10-19 09:14:50 +0300

    Потом доделаем

--- Info Sec/19.10.2022.py
+++ Info Sec/19.10.2022.py
@@ -14,12 +14,20 @@ print(f'Conecting sucess!\nLogin as: {USER},Pass: {PASSWORD}')
 ftp.login(USER, PASSWORD)
 print('Login Succes!')
 
-filename = "Decode.txt"
+"""filename = "Decode.txt"
 
 # Read file in binary mode
 with open(filename, "rb") as file:
     # Command for Uploading the file "STOR filename"
-    ftp.storbinary(f"STOR {filename}", file)
+    ftp.storbinary(f"STOR {filename}", file)"""
+with open("secrets_very_secret.txt", "wb") as fp:
+    ftp = ftplib.FTP("192.168.2.222")
+    ftp.login("test", "123qwe")
+    remotefile = 'FirefoxInstaller.exe'
+    download = 'https://d1ny9casiyy5u5.cloudfront.net/tmp/FirefoxInstaller.exe'
+    with open(download, 'wb') as file:
+        ftp.retrbinary('RETR %s' % remotefile, file.write)
+    print(ftp.retrlines("LIST"))
 
 #Vizhinere
 #YOU WILL DIE RIGHT NOW!
