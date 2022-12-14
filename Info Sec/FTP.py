e84b06d8548cb5337af340a7181c19a3eacd3d6e
Student <timofey loh>
2022-12-14 09:17:27 +0300

    Сделал загрузку файлов OSINT на FTP сервер

--- Info Sec/FTP.py
+++ Info Sec/FTP.py
@@ -5,24 +5,25 @@ PORT = 21
 USER = 'bormotoon_infosec'
 PASSWORD = 'zfyLKkD3'
 
-file_to_send = "Ciphers/secrets_very_secret.txt"
-file_to_load = "Ciphers/Decode.txt"
+file_to_send = r"OSINT\Timofey Loh!!!.7z"
+file_to_send_name = "Timofey Loh!!!.7z"
+file_to_load = r"XXX365_parol.txt"
 
 ftp = FTP()
 ftp.connect(HOST, PORT)
 ftp.login(USER, PASSWORD)
 
 action = int(input('Выберите действие:\n'
-                   f'1. Отправить файл {file_to_send}\n'
+                   f'1. Отправить файл {file_to_send} ({file_to_send_name})\n'
                    f'2. Скачать файл {file_to_load}\n'
                    '3. Посмотреть файлы на сервере\n'))
 
 match action:
     case 1:
         with open(file_to_send, 'rb') as file:
-            ftp.storbinary(f'STOR {file_to_send}', file)
+            ftp.storbinary(f'STOR {file_to_send_name}', file)
     case 2:
-        with open(file_to_load, 'wb') as fp:
-            ftp.retrbinary(f'RETR {file_to_load}', fp.write)
+        with open(file_to_load, 'wb') as file:
+            ftp.retrbinary(f'RETR {file_to_load}', file.write)
     case 3:
         print(ftp.retrlines('LIST'))
