393339be08fce6281b7202a06ce3ad44faf7ebbb
Student <lady.khodina@yandex.ru>
2023-01-17 14:13:30 +0300

    Сделал бота

--- Python/telegram_bot/main.py
+++ Python/telegram_bot/main.py
@@ -4,12 +4,35 @@ from telebot import types
 bot = telebot.TeleBot('5827315252:AAG3MaPfzVEPFkaawp7K53lF6xpNx0RCi70')
 
 
+commands = {
+    'Привет!': ('text', 'Здарова'),
+    'help': ('text', 'Типа помощь'),
+    'timothy': ('anim', 'https://thumbs.gfycat.com/UnrulyThriftyHedgehog-size_restricted.gif')
+}
+
+
 @bot.message_handler(commands=['start'])
 def start(msg: types.Message):
-    markup = types.InlineKeyboardMarkup()
-    btn = types.InlineKeyboardButton(text='На сайт!', url='https://педобраз.рф')
-    markup.add(btn)
+    markup = types.ReplyKeyboardMarkup()
+    for command in commands.keys():
+        print(command)
+        btn = types.KeyboardButton(text=command)
+        markup.add(btn)
     bot.send_message(msg.chat.id, 'Привет!', reply_markup=markup)
 
 
-bot.infinity_polling()
+@bot.message_handler(content_types=['text'])
+def get_text_messages(msg: types.Message):
+    answer = commands.get(msg.text)
+    if answer is None:
+        bot.send_message(msg.chat.id, 'Не знаю такой команды')
+
+    match answer[0]:
+        case 'text':
+            bot.send_message(msg.chat.id, answer[1])
+        case 'anim':
+            bot.send_animation(msg.chat.id, answer[1])
+
+
+
+bot.polling()
