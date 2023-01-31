dd25727188de2a3bfbb75b39ced79b8bece66950
Student <lady.khodina@yandex.ru>
2023-01-31 14:02:58 +0300

    бот

---
+++ Python/bot/main.py
@@ -0,0 +1,38 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5827315252:AAG3MaPfzVEPFkaawp7K53lF6xpNx0RCi70')
+
+commands = {
+    'Привет!':
+        lambda msg: bot.send_message(msg.chat.id, 'Здравствуйте'),
+    'help':
+        lambda msg: bot.send_message(msg.chat.id, 'Помощь'),
+    'timothy':
+        lambda msg: bot.send_animation(msg.chat.id, 'https://thumbs.gfycat.com/UnrulyThriftyHedgehog-size_restricted.gif'),
+    'Пришли картинку':
+        lambda msg: bot.send_photo(msg.chat.id, 'https://cdnn21.img.ria.ru/images/07e5/06/18/1738448523_0:89:864:575_1920x0_80_0_0_7541a4a6d36edb667d2de032b8aefc66.jpg'),
+    'Пришли стикер':
+        lambda msg: bot.send_sticker(msg.chat.id, 'CAACAgIAAxkBAAEHTe5jxoeGyDoZG1NNs_pxgYj1xxxDBwACzxIAArMK0Uv0uoqGrkIZMS0E'),
+}
+
+@bot.message_handler(commands=['start'])
+def start(msg: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    for command in commands.keys():
+        print(command)
+        btn = types.KeyboardButton(text=command)
+        markup.add(btn)
+    bot.send_message(msg.chat.id, 'Привет!', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(msg: types.Message):
+    answer = commands.get(msg.text)
+    if answer is None:
+        bot.send_message(msg.chat.id, 'Такой команды не существует')
+        return
+    answer(msg)
+
+
+bot.polling()
---
+++ Python/bot/main2.py
@@ -0,0 +1,37 @@
+import os
+
+import telebot
+from google.cloud import dialogflow_v2
+from telebot import types
+
+bot = telebot.TeleBot('5827315252:AAG3MaPfzVEPFkaawp7K53lF6xpNx0RCi70')
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'papupipapopubot-mfps-bb320d66ef80.json'
+
+DIALOGFLOW_PROJECT_ID = 'papupipapopubot-mfps'
+DIALOGFLOW_LANGUAGE_CODE = 'ru'
+SESSION_ID = 'me'
+
+session_client = dialogflow_v2.SessionsClient()
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+
+def detect_intent_text(text):
+    text_input = dialogflow_v2.types.TextInput(text=text, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow_v2.types.QueryInput(text=text_input)
+    response = session_client.detect_intent(session=session, query_input=query_input)
+    return response
+
+
+@bot.message_handler(commands=['start'])
+def start_message(msg: types.Message):
+    bot.send_message(msg.chat.id, 'Начало')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(msg: types.Message):
+    response = detect_intent_text(msg.text)
+    bot.reply_to(msg, response.query_result.fulfillment_text)
+
+
+bot.polling()
